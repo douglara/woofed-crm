22b5f0e89dbb60784c2fe135a25711e8c5849a81
Douglas Lara <douglasdelaras@gmail.com>
2023-02-27 10:08:40 -0300

    Fixes

--- app/use_cases/accounts/apps/wpp_connects/create_qr_code.rb
+++ app/use_cases/accounts/apps/wpp_connects/create_qr_code.rb
@@ -1,7 +1,8 @@
-include Rails.application.routes.url_helpers
+
 
 class Accounts::Apps::WppConnects::CreateQrCode
-
+  include Rails.application.routes.url_helpers
+  
   def self.call(wpp_connect_id)
     wpp_connect = Apps::WppConnect.find(wpp_connect_id)
     wpp_connect.session = "session_#{DateTime.now.to_i}"
