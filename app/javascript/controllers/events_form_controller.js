31a86d6a60539408395facc4f4b2be362e51c627
Douglas Lara <douglasdelaras@gmail.com>
2023-09-06 00:07:07 -0300

    UI improves

--- app/javascript/controllers/events_form_controller.js
+++ app/javascript/controllers/events_form_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "stimulus"
 import lucide from "lucide/dist/umd/lucide"
+import { animate } from "motion"
 
 export default class extends Controller {
   static targets = ['kindNone', 'kindNote', 'kindActivity']
@@ -8,19 +9,44 @@ export default class extends Controller {
     lucide.createIcons();
   }
 
-  selectNote() {
+  selectNone() {
     this.hiddenAll()
-    this.kindNoteTarget.hidden = false
+    this.kindNoneTarget.hidden = false
+    this.kindNoneTarget.classList.add('events-form-active')
   }
 
-  selectActivity() {
+  selectNote(e) {
+    this.hiddenAll()
+    this.kindNoteTarget.hidden = false
+    this.kindNoteTarget.classList.add('events-form-active')
+    e.currentTarget.classList.add('btn-active')
+  }
+
+  selectActivity(e) {
     this.hiddenAll()
     this.kindActivityTarget.hidden = false
+    this.kindActivityTarget.classList.add('events-form-active')
+    e.currentTarget.classList.add('btn-active')
   }
 
   hiddenAll() {
-    this.kindNoneTarget.hidden = true
-    this.kindNoteTarget.hidden = true
-    this.kindActivityTarget.hidden = true
+    this.disableBtn()
+    const activeElement = this.element.querySelector(".events-form-active")
+    activeElement.hidden = true
+    activeElement.classList.remove('events-form-active')
+
+    // animate(
+    //   activeElement,
+    //   { x: [0, -20], filter: [ "blur(10px)", "blur(0px)"], opacity: [1, 0]},
+    //   { duration: 0.3 }
+    // )
+  }
+
+  disableBtn() {
+    try {
+      const btnActive = this.element.querySelector(".btn-active")
+      btnActive.classList.remove('btn-active')
+    }
+    catch { }
   }
 }
