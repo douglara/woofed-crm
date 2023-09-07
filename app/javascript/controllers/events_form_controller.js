ed26a2a9fb90323cffd498e657451b3e0d12cbd4
Douglas Lara <douglasdelaras@gmail.com>
2023-09-07 17:10:13 -0300

    Add animation

--- app/javascript/controllers/events_form_controller.js
+++ app/javascript/controllers/events_form_controller.js
@@ -3,50 +3,63 @@ import lucide from "lucide/dist/umd/lucide"
 import { animate } from "motion"
 
 export default class extends Controller {
-  static targets = ['kindNone', 'kindNote', 'kindActivity']
+  static targets = ['kindNone', 'kindNote', 'kindActivity', 'forms']
 
   connect() {
     lucide.createIcons();
   }
 
-  selectNone() {
-    this.hiddenAll()
-    this.kindNoneTarget.hidden = false
-    this.kindNoneTarget.classList.add('events-form-active')
+  selectActivity(e) {
+    const elementToInactive = this.formsTarget.querySelector("div[data-events-form-target]:not([hidden])")
+    const elementToActive = this.kindActivityTarget
+    this.changeForm(elementToActive, elementToInactive)
+
+    const btnToActive = e.currentTarget
+    this.changeBtn(btnToActive)
   }
 
   selectNote(e) {
-    this.hiddenAll()
-    this.kindNoteTarget.hidden = false
-    this.kindNoteTarget.classList.add('events-form-active')
-    e.currentTarget.classList.add('btn-active')
+    const elementToInactive = this.formsTarget.querySelector("div[data-events-form-target]:not([hidden])")
+    const elementToActive = this.kindNoteTarget
+    this.changeForm(elementToActive, elementToInactive)
+
+    const btnToActive = e.currentTarget
+    this.changeBtn(btnToActive)
   }
 
-  selectActivity(e) {
-    this.hiddenAll()
-    this.kindActivityTarget.hidden = false
-    this.kindActivityTarget.classList.add('events-form-active')
-    e.currentTarget.classList.add('btn-active')
+  selectNone() {
+    const elementToInactive = this.formsTarget.querySelector("div[data-events-form-target]:not([hidden])")
+    const elementToActive = this.kindNoneTarget
+    this.changeForm(elementToActive, elementToInactive)
   }
 
-  hiddenAll() {
-    this.disableBtn()
-    const activeElement = this.element.querySelector(".events-form-active")
-    activeElement.hidden = true
-    activeElement.classList.remove('events-form-active')
-
-    // animate(
-    //   activeElement,
-    //   { x: [0, -20], filter: [ "blur(10px)", "blur(0px)"], opacity: [1, 0]},
-    //   { duration: 0.3 }
-    // )
-  }
-
-  disableBtn() {
+  changeBtn(btnToActive) {
     try {
       const btnActive = this.element.querySelector(".btn-active")
       btnActive.classList.remove('btn-active')
     }
     catch { }
+    btnToActive.classList.add('btn-active')
+  }
+
+  changeForm(elementToActive, elementToInactive) {
+    try {
+      elementToInactive.classList.remove('events-form-active')
+      elementToInactive.hidden = true
+      animate(
+        elementToInactive,
+        { x: [0, -20], filter: [ "blur(10px)", "blur(0px)"], opacity: [1, 0]},
+        { duration: 0.3 }
+      )  
+    } catch { }
+    try {
+      elementToActive.classList.add('events-form-active')
+      elementToActive.hidden = false
+      animate(
+        elementToActive,
+        { x: [-20, 0], filter: [ "blur(10px)", "blur(0px)"], opacity: [0, 1]},
+        { duration: 0.3 }
+      )
+    } catch { }
   }
 }
