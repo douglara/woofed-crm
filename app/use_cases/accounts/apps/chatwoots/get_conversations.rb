358999b13bacffc8a8e4cdff78057b4d3ce36407
Douglas Lara <douglasdelaras@gmail.com>
2023-10-18 00:52:04 -0300

    Implement use cases

---
+++ app/use_cases/accounts/apps/chatwoots/get_conversations.rb
@@ -0,0 +1,19 @@
+class Accounts::Apps::Chatwoots::GetConversations
+  def self.call(chatwoot, contact_id, inbox_id)
+    request = Faraday.post(
+      "#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/conversations/filter?page=1",
+      build_body(contact_id, inbox_id),
+      chatwoot.request_headers
+    )
+    return { ok: JSON.parse(request.body)['payload'] }
+  end
+
+  def self.build_body(contact_id, inbox_id)
+    {
+      "payload":[
+          {"attribute_key": "contact_id","attribute_model": "standard","filter_operator": "equal_to","values": ["#{contact_id}"],"query_operator": "and"},
+          {"attribute_key": "inbox_id","filter_operator": "equal_to","values":[inbox_id],"custom_attribute_type": ""}
+      ]    
+    }
+  end
+end
