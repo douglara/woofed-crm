6ed6202780ab1e2be312eaa3b8c42aa02aec04d4
Douglas Lara <douglasdelaras@gmail.com>
2023-10-27 09:08:11 -0300

    Allow development any hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.hosts = nil
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
