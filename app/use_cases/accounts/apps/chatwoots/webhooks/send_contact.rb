0cce64e0719c10bba3bcfd6b333b45ecb4df044a
Douglas Lara <douglasdelaras@gmail.com>
2023-10-31 20:31:51 -0300

    Code improves

--- app/use_cases/accounts/apps/chatwoots/webhooks/send_contact.rb
+++ app/use_cases/accounts/apps/chatwoots/webhooks/send_contact.rb
@@ -1,19 +1,25 @@
 class Accounts::Apps::Chatwoots::Webhooks::SendContact
   def self.call(chatwoot, contact)
-    request_body = {
+    request = Faraday.post(
+      "#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/contacts",
+      build_body(contact),
+      chatwoot.request_headers
+    )
+    response_body = JSON.parse(request.body) rescue {}
+  
+    if request.status == 200 && response_body['id'].present?
+      return { ok: contact }
+    else
+      return { error: request.body }
+    end
+  end
+
+  def self.build_body(contact)
+    {
       "name": contact['full_name'],
       "email": contact['email'],
       "phone_number": contact['phone'],
-      # "avatar": "string",
-      # "avatar_url": "string",
-      # "identifier": "string",
       "custom_attributes": contact['custom_attributes']
-      }
-    request = Faraday.post("#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/contacts",
-    request_body.to_json,
-    chatwoot.request_headers
-    )
-    byebug
-    return { ok: contact }
+    }.to_json
   end
 end
--- spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/send_contact_spec.rb
+++ spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/send_contact_spec.rb
@@ -3,18 +3,25 @@ require 'webmock/rspec'
 
 RSpec.describe Accounts::Apps::Chatwoots::Webhooks::SendContact, type: :request do
   describe 'success' do
-    let(:chatwoot) { create(:apps_chatwoots, :skip_validate) }
-    let(:contact_data) { attributes_for(:contact).to_json } 
-    let(:contact_response) { { ok: 'contact created', chatwoot_id: 715 }.to_json }
+    let(:account) { create(:account) }
+    let(:chatwoot) { create(:apps_chatwoots, :skip_validate, account: account) }
+    let(:contact) { create(:contact, account: account) }
+    let(:contact_response) { '{"account":{"id":3,"name":"Doug testes"},"additional_attributes":{"city":"","country":"","description":"","company_name":"","country_code":"","social_profiles":{"github":"","twitter":"","facebook":"","linkedin":"","instagram":""}},"avatar":"","custom_attributes":{},"email":"roberiojr@gmail.com","id":130,"identifier":null,"name":"Roberio","phone_number":"+5573991817179","thumbnail":"","event":"contact_created"}' }
 
     it 'sends contact data to Chatwoot API' do
       stub_request(:post, "#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/contacts")
         .to_return(body: contact_response, status: 200, headers: { 'Content-Type' => 'application/json' })
 
-      expect {
-        result = Accounts::Apps::Chatwoots::Webhooks::SendContact.call(chatwoot, contact_data)
-        expect(result.key?(:ok)).to eq(true)
-      }
+      result = Accounts::Apps::Chatwoots::Webhooks::SendContact.call(chatwoot, contact)
+      expect(result.key?(:ok)).to eq(true)
+    end
+
+    it 'invalid contact params' do
+      stub_request(:post, "#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/contacts")
+        .to_return(body: '', status: 400, headers: { 'Content-Type' => 'application/json' })
+
+      result = Accounts::Apps::Chatwoots::Webhooks::SendContact.call(chatwoot, contact)
+      expect(result.key?(:error)).to eq(true)
     end
   end
 end
