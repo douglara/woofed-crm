0f92da5d38b344fc6bac7a411a7233efaa6d4f7d
Douglas Lara <douglasdelaras@gmail.com>
2023-11-07 08:00:03 -0300

    Add Sidekiq logger

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,13 @@ require 'sidekiq/web'
 
 redis_max_connections = 12
 
+class RailsLoggerSidekiqServerMiddleware
+  def call(worker, job_options, queue)
+    tags = [worker.class.name, worker.jid]
+    Rails.logger.tagged(*tags) { yield }
+  end
+end
+
 Sidekiq.configure_client do |config|
   config.redis = {
       url: ENV['REDIS_URL'] || ENV['REDISCLOUD_URL'],
@@ -10,8 +17,16 @@ Sidekiq.configure_client do |config|
   }
 end
 
-
 Sidekiq.configure_server do |config|
+  config.server_middleware do |chain|
+    chain.add RailsLoggerSidekiqServerMiddleware
+  end
+
+  config.death_handlers << ->(job, ex) do
+    Rails.logger.error("Error #{job['class']} #{job["jid"]} just died with error #{ex.message}.")
+    Rails.logger.error(job.inspect)
+  end
+
   config.redis = {
     url: ENV['REDIS_URL'] || ENV['REDISCLOUD_URL'],
     size: redis_max_connections
@@ -19,7 +34,7 @@ Sidekiq.configure_server do |config|
 
   if ENV['PREVIEW_APP'].present?
     @activity_kind_whatsapp ||= ActivityKind.find_by_key('whatsapp')
-    Faraday.get("#{@activity_kind_whatsapp['settings']['endpoint_url']}")  
+    Faraday.get("#{@activity_kind_whatsapp['settings']['endpoint_url']}")
   end
   FlowItems::ActivitiesKinds::WpConnect::Connection::StartAll.call()
 end
