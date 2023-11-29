ce5aafafca5183e684bd9887c8f6c3dfd0b46ef8
Douglas Lara <douglasdelaras@gmail.com>
2023-11-29 00:34:00 -0300

    Fix file indentation

--- spec/controllers/accounts/contacts_controller_spec.rb
+++ spec/controllers/accounts/contacts_controller_spec.rb
@@ -1,104 +1,104 @@
 require 'rails_helper'
 
 RSpec.describe Accounts::ContactsController, type: :request do
-    let!(:account) { create(:account) }
-    let!(:user) { create(:user, account: account) }
-    let!(:contact) { create(:contact, account: account) }
+	let!(:account) { create(:account) }
+	let!(:user) { create(:user, account: account) }
+	let!(:contact) { create(:contact, account: account) }
 
-    context 'when it is an unauthenticated user' do
-        let!(:params) { { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '+5522998813788', account_id: account.id } } }
-        
-        it 'returns unauthorized' do
-        expect { post "/accounts/#{account.id}/contacts", params: params }.not_to change(Contact, :count)
-        expect(response).to redirect_to(new_user_session_path)
-        end
-    end
+	context 'when it is an unauthenticated user' do
+		let!(:params) { { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '+5522998813788', account_id: account.id } } }
 
-    context "POST #create" do
-        before do
-        sign_in(user)
-        end
+		it 'returns unauthorized' do
+			expect { post "/accounts/#{account.id}/contacts", params: params }.not_to change(Contact, :count)
+			expect(response).to redirect_to(new_user_session_path)
+		end
+	end
 
-        let!(:params) { { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '+5522998813788', account_id: account.id } } }
+	context "POST #create" do
+		before do
+			sign_in(user)
+		end
 
-        it "create contact" do
-            expect do
-                post "/accounts/#{account.id}/contacts", params: params
-            end.to change(Contact, :count).by(1)
-            
-            expect(response).to have_http_status(302)
-        end
+		let!(:params) { { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '+5522998813788', account_id: account.id } } }
 
-        context 'not create a new contact' do
-            it 'when full_name is blank' do
-                params = { contact: { full_name: '', email: 'yukioarie@gmail.com', phone: '+5522998813788', account_id: account.id } }
-                
-                expect do
-                post "/accounts/#{account.id}/contacts", params: params
-                end.to change(Contact, :count).by(0)
+		it "create contact" do
+			expect do
+				post "/accounts/#{account.id}/contacts", params: params
+			end.to change(Contact, :count).by(1)
 
-                expect(response.body).to include("Nome completo não pode ficar em branco")
-                expect(response).to have_http_status(:unprocessable_entity)
-            end
+			expect(response).to have_http_status(302)
+		end
 
-            it 'when full_name is nil' do
-                params = { contact: { full_name: nil, email: 'yukioarie@gmail.com', phone: '+5522998813788', account_id: account.id } }
-                
-                expect do
-                post "/accounts/#{account.id}/contacts", params: params
-                end.to change(Contact, :count).by(0)
+		context 'not create a new contact' do
+			it 'when full_name is blank' do
+				params = { contact: { full_name: '', email: 'yukioarie@gmail.com', phone: '+5522998813788', account_id: account.id } }
 
-                expect(response.body).to include("Nome completo não pode ficar em branco")
-                expect(response).to have_http_status(:unprocessable_entity)
-            end
+				expect do
+				post "/accounts/#{account.id}/contacts", params: params
+				end.to change(Contact, :count).by(0)
 
-            context "when phone is invalid" do
-                it 'when phone is more than 15 characters' do
-                params = { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '+552299881378888889', account_id: account.id } }
-                
-                expect do
-                    post "/accounts/#{account.id}/contacts", params: params
-                end.to change(Contact, :count).by(0)
+				expect(response.body).to include("Nome completo não pode ficar em branco")
+				expect(response).to have_http_status(:unprocessable_entity)
+			end
 
-                expect(response.body).to include("Telefone (celular) Número inválido")            
-                expect(response).to have_http_status(:unprocessable_entity)
-                end
+			it 'when full_name is nil' do
+				params = { contact: { full_name: nil, email: 'yukioarie@gmail.com', phone: '+5522998813788', account_id: account.id } }
 
-                it 'when phone starts with +0' do
-                params = { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '+052299881378888889', account_id: account.id } }
-                
-                expect do
-                    post "/accounts/#{account.id}/contacts", params: params
-                end.to change(Contact, :count).by(0)
+				expect do
+					post "/accounts/#{account.id}/contacts", params: params
+				end.to change(Contact, :count).by(0)
 
-                expect(response.body).to include("Telefone (celular) Número inválido")  
-                expect(response).to have_http_status(:unprocessable_entity)
-                end
+				expect(response.body).to include("Nome completo não pode ficar em branco")
+				expect(response).to have_http_status(:unprocessable_entity)
+			end
 
-                it 'when phone doesnt start with +' do
-                params = { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '052299881378888889', account_id: account.id } }
-                
-                expect do
-                    post "/accounts/#{account.id}/contacts", params: params
-                end.to change(Contact, :count).by(0)
+			context "when phone is invalid" do
+				it 'when phone is more than 15 characters' do
+					params = { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '+552299881378888889', account_id: account.id } }
 
-                expect(response.body).to include("Telefone (celular) Número inválido")  
-                expect(response).to have_http_status(:unprocessable_entity)
-                end
-            end
-        end
-    end
+					expect do
+						post "/accounts/#{account.id}/contacts", params: params
+					end.to change(Contact, :count).by(0)
 
-    context "GET #new" do
-        before do
-            sign_in(user)
-        end
+					expect(response.body).to include("Telefone (celular) Número inválido")
+					expect(response).to have_http_status(:unprocessable_entity)
+				end
 
-        it "create a new contact" do
-            get "/accounts/#{account.id}/contacts"
-            expect(response).to have_http_status(200)
-        end
-    end  
+				it 'when phone starts with +0' do
+					params = { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '+052299881378888889', account_id: account.id } }
+
+					expect do
+						post "/accounts/#{account.id}/contacts", params: params
+					end.to change(Contact, :count).by(0)
+
+					expect(response.body).to include("Telefone (celular) Número inválido")
+					expect(response).to have_http_status(:unprocessable_entity)
+				end
+
+				it 'when phone doesnt start with +' do
+					params = { contact: { full_name: 'Yukio Arie', email: 'yukioarie@gmail.com', phone: '052299881378888889', account_id: account.id } }
+
+					expect do
+						post "/accounts/#{account.id}/contacts", params: params
+					end.to change(Contact, :count).by(0)
+
+					expect(response.body).to include("Telefone (celular) Número inválido")
+					expect(response).to have_http_status(:unprocessable_entity)
+				end
+			end
+		end
+	end
+
+	context "GET #new" do
+		before do
+			sign_in(user)
+		end
+
+		it "create a new contact" do
+			get "/accounts/#{account.id}/contacts"
+			expect(response).to have_http_status(200)
+		end
+	end
 
 #   context "DELETE #destroy" do
 #     before do
@@ -111,5 +111,5 @@ RSpec.describe Accounts::ContactsController, type: :request do
 #       expect(Contact.find_by(id: contact.id)).to be_nil
 #       expect(response).to have_http_status(302)
 #     end
-#   end  
+#   end
 end
