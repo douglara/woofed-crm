03ae84d18890414d9665ee410d5ee534c4506318
Douglas Lara <douglasdelaras@gmail.com>
2023-12-06 09:21:00 -0300

    Rename tests

--- spec/integration/use_cases/accounts/apps/chatwoots/search_contact_spec.rb
+++ spec/integration/use_cases/accounts/apps/chatwoots/search_contact_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 require 'webmock/rspec'
 
 RSpec.describe Accounts::Apps::Chatwoots::ExportContact, type: :request do
-  describe 'success' do
+  describe '.call' do
     let(:account) { create(:account) }
     let(:chatwoot) { create(:apps_chatwoots, :skip_validate, account: account) }
     let(:contact) { create(:contact, account: account) }
     let(:response) { File.read("spec/integration/use_cases/accounts/apps/chatwoots/search_contact.json") }
 
-    it 'Chatwoot API search contact' do
+    it 'should return contact' do
       stub_request(:get, "#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/contacts/search").with(
         query: { q: contact[:email] },
         headers: chatwoot.request_headers
@@ -20,14 +20,14 @@ RSpec.describe Accounts::Apps::Chatwoots::ExportContact, type: :request do
       expect(result["name"]).to eq(contact[:full_name])
     end
 
-    it 'Chatwoot API seach contact not found' do
+    it 'should not found contact' do
       stub_request(:get, "#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/contacts/search").with(
         query: { q: 'yuki@email.com' },
         headers: chatwoot.request_headers
-      )
-        .to_return(body: '{"meta":{"count":0,"current_page":"1"},"payload":[]}', status: 200, headers: { 'Content-Type' => 'application/json' })
-        result = Accounts::Apps::Chatwoots::SearchContact.call(chatwoot, 'yuki@email.com')
-        expect(result.key?(:error)).to eq(true)
+      ).to_return(body: '{"meta":{"count":0,"current_page":"1"},"payload":[]}', status: 200, headers: { 'Content-Type' => 'application/json' })
+
+      result = Accounts::Apps::Chatwoots::SearchContact.call(chatwoot, 'yuki@email.com')
+      expect(result.key?(:error)).to eq(true)
     end
   end
 end
