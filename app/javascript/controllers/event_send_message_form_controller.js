c7e685ef74dd1d6100b1430cc0304b1b5d725747
Yukio Arie <yukioarie@gmail.com>
2023-12-09 06:38:24 -0300

    Adjust events forms

--- app/javascript/controllers/event_send_message_form_controller.js
+++ app/javascript/controllers/event_send_message_form_controller.js
@@ -1,22 +1,29 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["scheduledAtForm", "radioButtonDoneFalse", "radioButtonDoneTrue", 'checkBoxAutoDone', "dateFieldScheduletAt"]
+    static targets = ["scheduledAtForm", "radioButtonSendNowFalse", "radioButtonSendNowTrue", 'checkBoxAutoDone', "dateFieldScheduletAt"]
+    connect() {
+        this.scheduledAtFormTarget.classList.add("hidden");
+        this.checkBoxAutoDoneTarget.checked = true
+    }
 
     toggleRadioButton(event) {
         this.resetCheckRadioDiv() 
         event.currentTarget.ariaChecked = 'true'
         var doneInput = event.currentTarget.querySelector('input[type="radio"]');
         var done = doneInput.value === 'true';
-        this.radioButtonDoneTrueTarget.checked = done;
-        this.radioButtonDoneFalseTarget.checked = !done;
+        this.radioButtonSendNowTrueTarget.checked = done;
+        this.radioButtonSendNowFalseTarget.checked = !done;
         if (done) {
-            this.scheduledAtFormTarget.style.display = 'none';
-            this.checkBoxAutoDoneTarget.checked = false
+            // this.scheduledAtFormTarget.style.display = 'none';
+            this.scheduledAtFormTarget.classList.add("hidden");
+            this.checkBoxAutoDoneTarget.checked = true
             this.dateFieldScheduletAtTarget.value = ''
             
         } else {
-            this.scheduledAtFormTarget.style.display = '';
+            // this.scheduledAtFormTarget.style.display = '';
+            this.scheduledAtFormTarget.classList.remove("hidden");
+            this.checkBoxAutoDoneTarget.checked = false
         }
 
     }
