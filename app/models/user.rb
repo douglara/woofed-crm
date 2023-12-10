18907d00a9a8cf0d2e6fdd69a07af604d93d7612
Douglas Lara <douglasdelaras@gmail.com>
2023-12-09 23:52:21 -0300

    Add code spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,14 +29,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :account
+
   after_update_commit do
     broadcast_replace_later_to "users_#{account_id}", target: self, partial: '/accounts/users/user',
                                                       locals: { user: self }
   end
+
   after_create_commit do
     broadcast_append_later_to "users_#{account_id}", target: 'users', partial: '/accounts/users/user',
                                                      locals: { user: self }
   end
+
   after_destroy_commit do
     broadcast_remove_to "users_#{account_id}", target: self
   end
