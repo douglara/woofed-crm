05e844c8e0b55edf6a1b9d98bee57984ba8f4719
Douglas Lara <douglasdelaras@gmail.com>
2023-12-09 22:30:57 -0300

    Improve tests

--- spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/message/message_spec.rb
+++ spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/message/message_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe Accounts::Apps::Chatwoots::Webhooks::Events::Message, type: :requ
   describe 'success' do
     let(:account) { create(:account) }
     let(:chatwoot) { create(:apps_chatwoots, :skip_validate)}
-    let(:event) { File.read("spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/message/event.json") }
+    let(:event_message_sent) { File.read("spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/message/event_message_sent.json") }
+    let(:event_message_receive) { File.read("spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/message/event_message_receive.json") }
     let(:contact_response) { File.read("spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/response_contact.json") }
     let(:response_conversations) { File.read("spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/response_conversations.json") }
-    context "check message" do
+
+    context "receive event" do
       before do
         stub_request(:get, /contacts/).
         to_return(body: contact_response, status: 200, headers: {'Content-Type' => 'application/json'})
@@ -16,8 +18,9 @@ RSpec.describe Accounts::Apps::Chatwoots::Webhooks::Events::Message, type: :requ
         stub_request(:get, /conversations/).
         to_return(body: response_conversations, status: 200, headers: {'Content-Type' => 'application/json'})
       end
-      it 'check event chatwoot message' do    
-        result = Accounts::Apps::Chatwoots::Webhooks::Events::Message.call(chatwoot, JSON.parse(event))
+
+      it 'import event about chatwoot send message' do
+        result = Accounts::Apps::Chatwoots::Webhooks::Events::Message.call(chatwoot, JSON.parse(event_message_sent))
         expect(result.key?(:ok)).to eq(true)
         expect(Event.last.content.body.to_plain_text).to eq('Teste2')
         expect(Event.last.from_me).to eq(true)
@@ -25,6 +28,12 @@ RSpec.describe Accounts::Apps::Chatwoots::Webhooks::Events::Message, type: :requ
         expect(Event.last.kind).to eq('chatwoot_message')
         expect(Event.last.done_at).to eq("2023-07-26T01:59:54.994Z")
       end
-    end 
+      it 'import event about chatwoot receive message' do
+        result = Accounts::Apps::Chatwoots::Webhooks::Events::Message.call(chatwoot, JSON.parse(event_message_receive))
+        expect(result.key?(:ok)).to eq(true)
+        expect(Event.last.content.body.to_plain_text).to eq('teste receive')
+        expect(Event.last.from_me).to eq(false)
+      end
+    end
   end
 end
