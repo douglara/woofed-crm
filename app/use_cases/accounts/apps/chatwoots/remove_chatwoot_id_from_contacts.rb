785270e9f42edcac43a83154d20fbf0be6cda0ed
Yukio Arie <yukioarie@gmail.com>
2024-01-08 18:41:14 -0300

    Separe loops in batch to export and update contact

--- app/use_cases/accounts/apps/chatwoots/remove_chatwoot_id_from_contacts.rb
+++ app/use_cases/accounts/apps/chatwoots/remove_chatwoot_id_from_contacts.rb
@@ -1,10 +1,13 @@
 class Accounts::Apps::Chatwoots::RemoveChatwootIdFromContacts
-    def self.call(account)
-        account.contacts.where.not(additional_attributes: nil).each do |contact|
-            contact.additional_attributes.delete('chatwoot_id')
-            contact.save
-        end
-        return {ok: 'Contact chatwoot id removed successfully!' }
+  def self.call(account)
+    account.contacts.where("additional_attributes -> 'chatwoot_id' IS NOT NULL").find_in_batches(batch_size: 30) do |group|
+      group.each do |contact|
+        contact.additional_attributes.delete('chatwoot_id')
+        contact.save
+      end
+      sleep(30)
     end
+    return {ok: 'Contact chatwoot id was successfully removed.' }
+  end
 end
   
--- app/use_cases/accounts/apps/chatwoots/sync_export_contacts.rb
+++ app/use_cases/accounts/apps/chatwoots/sync_export_contacts.rb
@@ -1,14 +1,17 @@
 class Accounts::Apps::Chatwoots::SyncExportContacts
-    def self.call(account)
-        response = export_contacts(account)
-        return response
+  def self.call(account)
+    response = export_contacts(account)
+    return response
+  end
+  def self.export_contacts(account)
+    account.contacts.where("additional_attributes -> 'chatwoot_id' IS NULL").find_in_batches(batch_size: 30) do |group|
+      group.each do |contact|
+        Accounts::Apps::Chatwoots::ExportContact.call(account.apps_chatwoots.first, contact)
+      end
+      sleep(30)
     end
-    def self.export_contacts(account)
-        account.contacts.where("additional_attributes -> 'chatwoot_id' IS NULL").each do |contact|
-            Accounts::Apps::Chatwoots::ExportContact.call(account.apps_chatwoots.first, contact)
-        end
-        return {ok: 'Contacts exported successfully'}
-    end
+    return {ok: 'Contacts exported successfully'}
+  end
 end
 
   
