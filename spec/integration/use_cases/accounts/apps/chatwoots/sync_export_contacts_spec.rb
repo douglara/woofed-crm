5db7fe58e0f68afcddea3b6785d7f72197ef6a8c
Yukio Arie <yukioarie@gmail.com>
2024-01-09 01:06:31 -0300

    Improve sync_export_contacts_spec

--- spec/integration/use_cases/accounts/apps/chatwoots/sync_export_contacts_spec.rb
+++ spec/integration/use_cases/accounts/apps/chatwoots/sync_export_contacts_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 require 'webmock/rspec'
+require 'sidekiq/testing'
 
 RSpec.describe Accounts::Apps::Chatwoots::SyncExportContacts, type: :request do
   describe '.call' do
     let!(:account) { create(:account) }
     let!(:chatwoot) { create(:apps_chatwoots, :skip_validate, account: account) }
-    let!(:contact_1) { create(:contact, account: account, additional_attributes: {'chatwoot_id': 1}, full_name:'teste', email: 'emailteste@email.com') }
+    let!(:contact_1) do
+      create(:contact, account: account, additional_attributes: { 'chatwoot_id': 1 }, full_name: 'teste',
+                       email: 'emailteste@email.com')
+    end
     let!(:contact_2) { create(:contact, account: account) }
-    let(:contact_create_response) do
-        {
-          "payload": {
-            "contact": {
-              "additional_attributes": {},
-              "availability_status": "offline",
-              "email": "tim@maia.com",
-              "id": 368,
-              "name": "Tim Maia",
-              "phone_number": "",
-              "identifier": nil,
-              "thumbnail": "",
-              "custom_attributes": {
-                "CPF": "",
-                "Cpf": "",
-                "cep": "",
-                "Cnpj": "",
-                "TEste": "",
-                "hgfhfgh": "",
-                "Endereço de casa": ""
-              },
-              "created_at": 1701324831,
-              "contact_inboxes": []
+    let(:contact_update_response) do
+      {
+        "id": 368,
+        "payload": {
+          "contact": {
+            "additional_attributes": {},
+            "availability_status": 'offline',
+            "email": 'tim@maia.com',
+            "name": 'Tim Maia',
+            "phone_number": '',
+            "thumbnail": '',
+            "custom_attributes": {
+              "CPF": '',
+              "Cpf": '',
+              "cep": '',
+              "Cnpj": '',
+              "TEste": '',
+              "hgfhfgh": '',
+              "Endereço de casa": ''
             },
             "contact_inbox": {
               "inbox": nil,
@@ -37,17 +37,53 @@ RSpec.describe Accounts::Apps::Chatwoots::SyncExportContacts, type: :request do
             }
           }
         }
+      }
+    end
+    let(:contact_create_response) do
+      {
+        "payload": {
+          "contact": {
+            "additional_attributes": {},
+            "availability_status": 'offline',
+            "email": 'tim@maia.com',
+            "id": 368,
+            "name": 'Tim Maia',
+            "phone_number": '',
+            "identifier": nil,
+            "thumbnail": '',
+            "custom_attributes": {
+              "CPF": '',
+              "Cpf": '',
+              "cep": '',
+              "Cnpj": '',
+              "TEste": '',
+              "hgfhfgh": '',
+              "Endereço de casa": ''
+            },
+            "created_at": 1_701_324_831,
+            "contact_inboxes": []
+          },
+          "contact_inbox": {
+            "inbox": nil,
+            "source_id": nil
+          }
+        }
+      }
     end
 
     describe 'success' do
-      before do 
+      before do
         stub_request(:post, "#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/contacts")
-        .to_return(body: contact_create_response.to_json, status: 200, headers: { 'Content-Type' => 'application/json' })
+          .to_return(body: contact_create_response.to_json, status: 200, headers: { 'Content-Type' => 'application/json' })
+        stub_request(:put, "#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/contacts/#{contact_create_response[:payload][:contact][:id]}")
+          .to_return(body: contact_update_response.to_json, status: 200, headers: { 'Content-Type' => 'application/json' })
       end
       it 'Export contact data to Chatwoot API' do
-        result = Accounts::Apps::Chatwoots::SyncExportContacts.call(account)
-        expect(result.key?(:ok)).to eq(true)
+        Sidekiq::Testing.inline! do
+          Accounts::Apps::Chatwoots::SyncExportContactsWorker.perform_async(account.id)
+        end
         expect(account.contacts.count).to eq(2)
+        expect(account.contacts.where("additional_attributes -> 'chatwoot_id' IS NOT NULL").count).to eq(2)
       end
     end
   end
