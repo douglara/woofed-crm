a719d8cd061eb1cb809f198626ca4d58b815409e
Yukio Arie <yukioarie@gmail.com>
2024-01-10 09:21:25 -0300

    Replace find_in_batches to find_each in remove chatwoot id from contacts

--- app/use_cases/accounts/apps/chatwoots/remove_chatwoot_id_from_contacts.rb
+++ app/use_cases/accounts/apps/chatwoots/remove_chatwoot_id_from_contacts.rb
@@ -1,13 +1,10 @@
 class Accounts::Apps::Chatwoots::RemoveChatwootIdFromContacts
   def self.call(account)
-    account.contacts.where("additional_attributes -> 'chatwoot_id' IS NOT NULL").find_in_batches(batch_size: 30) do |group|
-      group.each do |contact|
-        contact.additional_attributes.delete('chatwoot_id')
-        contact.save
-      end
-      sleep(30)
+    account.contacts.where("additional_attributes -> 'chatwoot_id' IS NOT NULL").find_each do |contact|
+      contact.additional_attributes.delete('chatwoot_id')
+      contact.save
     end
-    return {ok: 'Contact chatwoot id was successfully removed.' }
+    return { ok: 'Contact chatwoot id was successfully removed.' }
   end
 end
   
