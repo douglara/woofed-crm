b652a0ef8c37d5a8a06c52d2b4d75f99e738176d
Yukio Arie <yukioarie@gmail.com>
2024-01-12 00:19:53 -0300

    Fix send_now use case

--- app/use_cases/accounts/contacts/events/send_now.rb
+++ app/use_cases/accounts/contacts/events/send_now.rb
@@ -1,19 +1,11 @@
 class Accounts::Contacts::Events::SendNow
-
   def self.call(event)
-    set_scheduled_at(event)
+    event.send_now = nil
     if event.kind == 'chatwoot_message'
+      event.update(scheduled_at: DateTime.current)
       Accounts::Apps::Chatwoots::Messages::DeliveryJob.perform_later(event.id)
     else
-      event.send_now = nil
       event.update(done: true)
-    end
-  end
-  def self.set_scheduled_at(event)
-    if event.scheduled_at.nil?
-      # event.send_now = nil
-      event.scheduled_at = Time.current
-      event.save
     end
   end
 end
