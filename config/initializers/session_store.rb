78a88306a9e19a57a91d38cfacf5329477adefb5
Douglas Lara <douglasdelaras@gmail.com>
2024-01-20 17:00:21 -0300

    Change session store configs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,4 @@ Rails.application.config.session_store :cookie_store,
   :key => '_woofedcrm_session',
   :domain => :all,
   :same_site => :none,
-  :secure => :true,
   :tld_length => 2 if Rails.env.production?
