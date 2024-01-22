831c6a2a4e343d796a3ccdbabf1a9fc5cef43e48
Douglas Lara <douglasdelaras@gmail.com>
2024-01-22 07:30:53 -0300

    Try change devise csrf config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -103,7 +103,7 @@ Devise.setup do |config|
   # avoid CSRF token fixation attacks. This means that, when using AJAX
   # requests for sign in and sign up, you need to get a new CSRF token
   # from the server. You can disable this option at your own risk.
-  # config.clean_up_csrf_token_on_authentication = true
+  config.clean_up_csrf_token_on_authentication = true
 
   # When false, Devise will not attempt to reload routes on eager load.
   # This can reduce the time taken to boot the app but if your application
