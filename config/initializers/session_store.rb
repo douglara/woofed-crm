deaad6a8b38dbd54ae1998a1bed532f70821d50a
Douglas Lara <douglasdelaras@gmail.com>
2024-01-22 21:30:54 -0300

    Fix configs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.config.session_store :cookie_store,
-  :key => "_#{ENV.fetch('DOMAIN', 'woofedcrm')}_session",
-  :domain => :all,
-  :same_site => :none,
-  :secure => :true,
-  :tld_length => 3
+                                       key: "_woofedcrm_#{Rails.env}_session",
+                                       domain: :all,
+                                       same_site: :none,
+                                       secure: true,
+                                       tld_length: 3 if Rails.env.production?
