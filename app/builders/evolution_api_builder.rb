e6002d3afe73cb5525a076678c7e82cfaf50a60c
Yukio Arie <yukioarie@gmail.com>
2024-01-25 17:08:17 -0300

    Fix evolution api builder endpoint_url

--- app/builders/evolution_api_builder.rb
+++ app/builders/evolution_api_builder.rb
@@ -8,7 +8,7 @@ class EvolutionApiBuilder
     @evolution_api = @user.account.apps_evolution_apis.new(@params)
     @evolution_api.instance = @evolution_api.generate_token('instance')
     @evolution_api.token = @evolution_api.generate_token('token')
-    @evolution_api.endpoint_url = ENV['EVOLUTION_API_ENDPOINT_TOKEN']
+    @evolution_api.endpoint_url = ENV['EVOLUTION_API_ENDPOINT']
     @evolution_api
   end
 end
