9ccacb6d35ab6b248b22d6b45b43bcaf6c8edc9d
Yukio <45243355+YukioArie@users.noreply.github.com>
2024-01-26 07:51:31 -0300

    Add create instance use case evolutionapi (#240)
    * Add model EvolutionApi
    
    * Add evolutionapi .env.sample
    
    * Add method request_headers to evolution_api model
    
    * Add use case create instance evolution api
    
    * Add tests to create instance evolution_api
    
    * Add generate_token name and update create instance with webhook
    
    * Add additional attributes to evolution_api
    
    * Add has_many evolution api to account model
    
    * Add evolution_api builder
    
    * Add method request instance headers
    
    * Add test to generate_token() evolution_api
    
    * Add evolution api variables to .env.github
    
    * Update env github
    
    * Update create response json test
    
    * Modify fields from evolution_api tables
    
    * Add validates fields from evolution_api model
    
    * Modify EvolutionApiBuilder
    
    * Fix use_case create instance evolution api
    
    * Fix evolution api builder endpoint_url
    
    * Fix evolution_api factory test
    
    * Remove unnecessary test from EvolutionApi model
    
    * Add url to receive evolution_api webhooks
    
    * Rename use case evolution api folder

---
+++ app/use_cases/accounts/apps/evolution_apis/instance/create.rb
@@ -0,0 +1,32 @@
+class Accounts::Apps::EvolutionApis::Instance::Create
+
+  def self.call(evolution_api)
+    request = Faraday.post(
+      "#{evolution_api.endpoint_url}/instance/create",
+      build_body(evolution_api).to_json,
+      {'apiKey': "#{ENV['EVOLUTION_API_ENDPOINT_TOKEN']}", 'Content-Type': 'application/json'}
+    )
+    if request.status == 201
+      return { ok: JSON.parse(request.body) }
+    else
+      return { error: JSON.parse(request.body) }
+    end
+  end
+
+  def self.build_body(evolution_api)
+    {
+      "instanceName": evolution_api.instance,
+      "token": evolution_api.token,
+      "qrcode": true,
+      "webhook": evolution_api.woofedcrm_webhooks_url,
+      "events": [
+        "QRCODE_UPDATED",
+        "MESSAGES_DELETE",
+        "SEND_MESSAGE",
+        "CONNECTION_UPDATE",
+        "MESSAGES_UPSERT",
+      ]
+    }
+  end
+
+end
