cbd494ceed84e68a7c8cc14bbd3590a53f602b14
Yukio Arie <yukioarie@gmail.com>
2024-01-27 00:50:38 -0300

    Update receive params from qrcode_updated event webhook

--- app/use_cases/accounts/apps/evolution_apis/webhooks/events/qrcode_updated.rb
+++ app/use_cases/accounts/apps/evolution_apis/webhooks/events/qrcode_updated.rb
@@ -1,6 +1,6 @@
 class Accounts::Apps::EvolutionApis::Webhooks::Events::QrcodeUpdated
-  def self.call(evolution_api, webhook)
-    evolution_api.update(additional_attributes: { 'qrcode': webhook['qrcode']['base64'],
+  def self.call(evolution_api, qrcode)
+    evolution_api.update(additional_attributes: { 'qrcode': qrcode,
                                                   'expiration_date': (Time.current + 50.seconds).to_s })
     { ok: evolution_api }
   end
--- app/use_cases/accounts/apps/evolution_apis/webhooks/process_webhook.rb
+++ app/use_cases/accounts/apps/evolution_apis/webhooks/process_webhook.rb
@@ -4,7 +4,7 @@ class Accounts::Apps::EvolutionApis::Webhooks::ProcessWebhook
 
     if webhook['webhook']['events'].include?('QRCODE_UPDATED')
       Accounts::Apps::EvolutionApis::Webhooks::Events::QrcodeUpdated.call(
-        evolution_api, webhook
+        evolution_api, webhook['qrcode']['base64']
       )
     end
 
