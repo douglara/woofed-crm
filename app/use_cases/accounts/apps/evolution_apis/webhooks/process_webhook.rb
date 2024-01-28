b98e86b148f2a9ca46f548bb8ee4738560f129ef
Yukio Arie <yukioarie@gmail.com>
2024-01-27 22:02:15 -0300

    Rename and refactor webhook tests

--- app/use_cases/accounts/apps/evolution_apis/webhooks/process_webhook.rb
+++ app/use_cases/accounts/apps/evolution_apis/webhooks/process_webhook.rb
@@ -1,17 +1,16 @@
 class Accounts::Apps::EvolutionApis::Webhooks::ProcessWebhook
   def self.call(webhook)
     evolution_api = Apps::EvolutionApi.find_by(instance: webhook['instance'])
-
     if webhook['event'] == 'qrcode.updated'
       Accounts::Apps::EvolutionApis::Webhooks::Events::QrcodeUpdated.call(
         evolution_api, webhook['data']['qrcode']['base64']
       )
     elsif webhook['event'] == 'connection.update'
-      if webhook['data']['statusReason'] == 200 && webhook['data']['state'] == 'open'
+      if webhook['data']['statusReason'] == '200' && webhook['data']['state'] == 'open'
         Accounts::Apps::EvolutionApis::Webhooks::Events::ConnectionCreated.call(evolution_api,
                                                                                 webhook['sender'].gsub(/\D/, ''))
 
-      elsif webhook['data']['statusReason'] == 401 && webhook['data']['state'] == 'close'
+      elsif webhook['data']['statusReason'] == '401' && webhook['data']['state'] == 'close'
         Accounts::Apps::EvolutionApis::Webhooks::Events::ConnectionDeleted.call(evolution_api)
       end
     end
--- spec/integration/use_cases/accounts/apps/evolution_api/webhooks/evolution_apis_controller_spec.rb
+++ spec/integration/use_cases/accounts/apps/evolution_api/webhooks/evolution_apis_controller_spec.rb
@@ -2,29 +2,36 @@ require 'rails_helper'
 require 'webmock/rspec'
 require 'sidekiq/testing'
 
-RSpec.describe Accounts::Apps::EvolutionApis::Webhooks::ProcessWebhookWorker, type: :request do
-  describe '.perform' do
-    let(:account) { create(:account) }
-    let(:evolution_api) { create(:apps_evolution_api, account: account) }
-    let(:qrcode_updated_webhook_event) do
-      File.read('spec/integration/use_cases/accounts/apps/evolution_api/webhooks/events/qrcode_updated_event.json')
-    end
-    let(:created_connection_event) do
-      File.read('spec/integration/use_cases/accounts/apps/evolution_api/webhooks/events/created_connection_event.json')
-    end
-    let(:deleted_connection_event) do
-      File.read('spec/integration/use_cases/accounts/apps/evolution_api/webhooks/events/deleted_connection_event.json')
-    end
-    let(:evolution_api_additional_attributes) { evolution_api.reload.additional_attributes }
+RSpec.describe Apps::EvolutionApisController, type: :request do
 
+  let(:account) { create(:account) }
+  let(:evolution_api) { create(:apps_evolution_api, account: account) }
+  let(:qrcode_updated_webhook_event) { load_webhook_event('qrcode_updated_event.json') }
+  let(:created_connection_event) { load_webhook_event('created_connection_event.json') }
+  let(:deleted_connection_event) { load_webhook_event('deleted_connection_event.json') }
+  let(:evolution_api_additional_attributes) { evolution_api.reload.additional_attributes }
+
+  def load_webhook_event(filename)
+    File.read(Rails.root.join('spec/integration/use_cases/accounts/apps/evolution_api/webhooks/events', filename))
+  end
+
+  def post_webhook(event_data)
+    Sidekiq::Testing.inline! { post '/apps/evolution_apis/webhooks', params: JSON.parse(event_data) }
+  end
+
+  def expect_success
+    expect(response).to have_http_status(200)
+    expect(JSON.parse(response.body)).to eq({ 'ok' => true })
+  end
+
+  describe 'POST /apps/evolution_apis/webhooks' do
     describe 'when is qrcode_update event' do
       context 'success' do
         it 'should update additional_attributes' do
           allow(Time).to receive(:current).and_return(Time.new(2024, 1, 27, 0, 0, 0))
           evolution_api
-          Sidekiq::Testing.inline! do
-            described_class.perform_async(qrcode_updated_webhook_event)
-          end
+          post_webhook(qrcode_updated_webhook_event)
+          expect_success
           expect(evolution_api.reload.additional_attributes).to eq({ 'qrcode' => 'qrcode',
                                                                      'expiration_date' => (Time.new(2024, 1, 27, 0, 0,
                                                                                                     0) + 50.seconds).to_s })
@@ -35,9 +42,8 @@ RSpec.describe Accounts::Apps::EvolutionApis::Webhooks::ProcessWebhookWorker, ty
       context 'success' do
         it 'should update evolution_api status and phone' do
           evolution_api.update(additional_attributes: { qrcode: 'qrcode', expiration_date: Time.current })
-          Sidekiq::Testing.inline! do
-            described_class.perform_async(created_connection_event)
-          end
+          post_webhook(created_connection_event)
+          expect_success
           expect(evolution_api.reload.connection_status).to eq('active')
           expect(evolution_api.reload.phone).to be_truthy
           expect(evolution_api.reload.additional_attributes).not_to include('qrcode', 'expired_date')
@@ -48,9 +54,8 @@ RSpec.describe Accounts::Apps::EvolutionApis::Webhooks::ProcessWebhookWorker, ty
       context 'success' do
         it 'should update evolution_api status and phone' do
           evolution_api.update(phone: '+5522999999999', connection_status: 'active')
-          Sidekiq::Testing.inline! do
-            described_class.perform_async(deleted_connection_event)
-          end
+          post_webhook(deleted_connection_event)
+          expect_success
           expect(evolution_api.reload.connection_status).to eq('inactive')
         end
       end
