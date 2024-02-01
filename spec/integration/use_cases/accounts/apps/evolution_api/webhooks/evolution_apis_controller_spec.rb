513d458c148ea8fa0c36db51a0987c0128511048
Yukio Arie <yukioarie@gmail.com>
2024-02-01 17:39:16 -0300

    Improve evolution_apis_controller_spec

--- spec/integration/use_cases/accounts/apps/evolution_api/webhooks/evolution_apis_controller_spec.rb
+++ spec/integration/use_cases/accounts/apps/evolution_api/webhooks/evolution_apis_controller_spec.rb
@@ -4,9 +4,6 @@ require 'sidekiq/testing'
 
 RSpec.describe Apps::EvolutionApisController, type: :request do
   let(:account) { create(:account) }
-  let(:evolution_api) { create(:apps_evolution_api, account: account) }
-  let(:evolution_api_connected) { create(:apps_evolution_api, :connected, account: account) }
-  let(:evolution_api_connecting) { create(:apps_evolution_api, :connecting, account: account) }
   let(:qrcode_updated_webhook_event) { load_webhook_event('qrcode_updated_event.json') }
   let(:created_connection_event) { load_webhook_event('created_connection_event.json') }
   let(:deleted_connection_event) { load_webhook_event('deleted_connection_event.json') }
@@ -26,35 +23,16 @@ RSpec.describe Apps::EvolutionApisController, type: :request do
   end
 
   describe 'POST /apps/evolution_apis/webhooks' do
-    describe 'when is qrcode_update event' do
-      context 'when evolution_api is connecting' do
+    describe 'when evolution_api is connecting' do
+      let!(:evolution_api_connecting) { create(:apps_evolution_api, :connecting, account: account) }
+      context 'when is qrcode_update event ' do
         it 'should update qrcode' do
-          evolution_api_connecting
           post_webhook(qrcode_updated_webhook_event)
           expect_success
           expect(evolution_api_connecting.reload.qrcode).to eq('qrcode')
         end
       end
-      context 'when evolution_api is disconnected' do
-        let!(:evolution_api) { create(:apps_evolution_api, account: account) }
-        it 'evolution_api should not be changed' do
-          post_webhook(qrcode_updated_webhook_event)
-          expect_success
-          expect(evolution_api.reload.changed?).to be_falsey
-        end
-      end
-      context 'when evolution_api is connected' do
-        let!(:evolution_api_connected) { create(:apps_evolution_api, :connected, account: account) }
-        it 'evolution_api should not be changed' do
-          post_webhook(qrcode_updated_webhook_event)
-          expect_success
-          expect(evolution_api_connected.reload.changed?).to be_falsey
-        end
-      end
-    end
-    describe 'when is created_connection event' do
-      let!(:evolution_api_connecting) { create(:apps_evolution_api, :connecting, account: account) }
-      context 'success' do
+      context 'when is created_connection event' do
         it 'should update evolution_api status, phone and qrcode' do
           post_webhook(created_connection_event)
           expect_success
@@ -63,39 +41,49 @@ RSpec.describe Apps::EvolutionApisController, type: :request do
           expect(evolution_api_connecting.qrcode).not_to be_present
         end
       end
-    end
-    describe 'when is deleted_connection event' do
-      before do
-
+      context 'when is deleted_connection event' do
+        it 'should update evolution_api status' do
+          stub_request(:delete, /delete/)
+          .to_return(body: delete_instance_response.to_json, status: 200, headers: {'Content-Type' => 'application/json'})
+          post_webhook(deleted_connection_event)
+          expect_success
+          expect(evolution_api_connecting.reload.connection_status).to be_truthy
+        end
       end
-      context 'success' do
-        context 'when evolution_api is connected' do
-          it 'should update evolution_api status' do
-            stub_request(:delete, /delete/)
-            .to_return(body: delete_instance_response.to_json, status: 200, headers: {'Content-Type' => 'application/json'})
-            evolution_api_connected
-            post_webhook(deleted_connection_event)
-            expect_success
-            expect(evolution_api_connected.reload.disconnected?).to be_truthy
-          end
+    end
+    describe 'when evolution_api is connected' do
+      let!(:evolution_api_connected) { create(:apps_evolution_api, :connected, account: account) }
+      context 'when is qrcode_update event' do
+        it 'evolution_api should not be changed' do
+          post_webhook(qrcode_updated_webhook_event)
+          expect_success
+          expect(evolution_api_connected.reload.changed?).to be_falsey
         end
-        context 'when evolution_api is disconnected' do
-          let!(:evolution_api) { create(:apps_evolution_api, account: account) }
-          it 'evolution_api should not be changed' do
-            post_webhook(deleted_connection_event)
-            expect_success
-            expect(evolution_api.reload.changed?).to be_falsey
-          end
+      end
+      context 'when is deleted_connection event' do
+        it 'should update evolution_api status' do
+          stub_request(:delete, /delete/)
+          .to_return(body: delete_instance_response.to_json, status: 200, headers: {'Content-Type' => 'application/json'})
+          post_webhook(deleted_connection_event)
+          expect_success
+          expect(evolution_api_connected.reload.disconnected?).to be_truthy
         end
-        context 'when evolution_api is connecting' do
-          it 'should update evolution_api status' do
-            stub_request(:delete, /delete/)
-            .to_return(body: delete_instance_response.to_json, status: 200, headers: {'Content-Type' => 'application/json'})
-            evolution_api_connected
-            post_webhook(deleted_connection_event)
-            expect_success
-            expect(evolution_api_connected.reload.connection_status).to be_truthy
-          end
+      end
+    end
+    describe 'when evolution_api is disconnected' do
+      let!(:evolution_api) { create(:apps_evolution_api, account: account) }
+      context 'when is qrcode_update event' do
+        it 'evolution_api should not be changed' do
+          post_webhook(qrcode_updated_webhook_event)
+          expect_success
+          expect(evolution_api.reload.changed?).to be_falsey
+        end
+      end
+      context 'when is deleted_connection event' do
+        it 'evolution_api should not be changed' do
+          post_webhook(deleted_connection_event)
+          expect_success
+          expect(evolution_api.reload.changed?).to be_falsey
         end
       end
     end
