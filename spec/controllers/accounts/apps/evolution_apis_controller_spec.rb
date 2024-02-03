27b41a4c09b04760d0be0137581d4ccaa5eed6ce
Yukio Arie <yukioarie@gmail.com>
2024-02-03 05:29:16 -0300

    Add tests for evolution_apis_controller

---
+++ spec/controllers/accounts/apps/evolution_apis_controller_spec.rb
@@ -0,0 +1,206 @@
+require 'rails_helper'
+require 'webmock/rspec'
+
+RSpec.describe Accounts::Apps::EvolutionApisController, type: :request do
+  let!(:account) { create(:account) }
+  let(:evolution_api) { create(:apps_evolution_api, account: account) }
+  let!(:user) { create(:user, account: account) }
+  let(:app_evolution_api_created) { Apps::EvolutionApi.first }
+  let(:valid_params){
+    { apps_evolution_api: {
+        name: 'woofed whatsapp'
+      }
+    }
+  }
+  let(:invalid_params){
+    { apps_evolution_api: {
+        name: '',
+      }
+    }
+  }
+
+  describe 'POST /accounts/{account_id}/apps/evolution_apis' do
+    let(:create_instance_response) { File.read('spec/integration/use_cases/accounts/apps/evolution_api/instance/create_response.json')}
+    before do
+      stub_request(:post, /instance/)
+        .to_return(body: create_instance_response, status: 201, headers: { 'Content-Type' => 'application/json' })
+    end
+    context 'when is unauthenticated user' do
+      it 'returns unauthorized' do
+        post "/accounts/#{account.id}/apps/evolution_apis", params: valid_params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    context 'when is authenticated user' do
+      before do
+        sign_in(user)
+      end
+      it 'create app evolution_apis' do
+        expect do
+          post "/accounts/#{account.id}/apps/evolution_apis", params: valid_params
+        end.to change(Apps::EvolutionApi, :count).by(1)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(account_apps_evolution_api_pair_qr_code_path(app_evolution_api_created.account, app_evolution_api_created.id))
+      end
+      it 'create app evolution_apis process failed' do
+        expect do
+          post "/accounts/#{account.id}/apps/evolution_apis", params: invalid_params
+        end.to change(Apps::EvolutionApi, :count).by(0)
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to include('Name não pode ficar em branco')
+      end
+    end
+  end
+  describe 'GET /accounts/{account_id}/apps/evolution_apis/new' do
+    context 'when is unauthenticated user' do
+      it 'returns unauthorized' do
+        get "/accounts/#{account.id}/apps/evolution_apis/new"
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    context 'when is authenticated user' do
+      before do
+        sign_in(user)
+      end
+      it 'should redirect to new apps evolution_apis page' do
+        get "/accounts/#{account.id}/apps/evolution_apis/new"
+        expect(response).to have_http_status(200)
+        expect(response.body).to include('Dados da conexão')
+      end
+    end
+  end
+
+  describe 'UPDATE /accounts/{account_id}/apps/evolution_apis/{evolution_api_id}' do
+    context 'when is unauthenticated user' do
+      it 'returns unauthorized' do
+        patch "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api.id}"
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    context 'when is authenticated user' do
+      before do
+        sign_in(user)
+      end
+      it 'update apps evoluiton_apis' do
+        patch "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api.id}", params: valid_params
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(edit_account_apps_evolution_api_path(account, evolution_api))
+        expect(flash[:notice]).to eq('Whatsapp updated successfully!')
+      end
+    end
+  end
+
+  describe 'GET /accounts/{account_id}/apps/evolution_apis/{evolution_api_id}/edit' do
+    context 'when is unauthenticated user' do
+      it 'returns unauthorized' do
+        get "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api.id}/edit"
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    context 'when is authenticated user' do
+      before do
+        sign_in(user)
+      end
+      it 'should redirect to edit evolution_apis page' do
+        get "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api.id}/edit"
+        expect(response).to have_http_status(200)
+        expect(response.body).to include('Dados da conexão')
+      end
+    end
+  end
+
+  describe 'GET /accounts/{account_id}/apps/evolution_apis' do
+
+    context 'when is unauthenticated user' do
+      it 'returns unauthorized' do
+        get "/accounts/#{account.id}/apps/evolution_apis"
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    context 'when is authenticated user' do
+      before do
+        sign_in(user)
+      end
+      context 'when there are evolution_api disconnected and connecting' do
+        let!(:evolution_api) { create(:apps_evolution_api, account: account) }
+        let!(:evolution_api_connecting) { create(:apps_evolution_api, :connecting ,account: account) }
+        it "should show connect button link" do
+          get "/accounts/#{account.id}/apps/evolution_apis"
+          expect(response).to have_http_status(200)
+          expect(response.body).to include(account_apps_evolution_api_pair_qr_code_path(account, evolution_api))
+          expect(response.body).to include(account_apps_evolution_api_pair_qr_code_path(account, evolution_api_connecting))
+        end
+      end
+      context 'when there is evolution_api connected' do
+        let!(:evolution_api_connected) { create(:apps_evolution_api, :connected ,account: account) }
+        it "should not show connect button link" do
+          get "/accounts/#{account.id}/apps/evolution_apis"
+          expect(response).to have_http_status(200)
+          expect(response.body).not_to include(account_apps_evolution_api_pair_qr_code_path(account, evolution_api_connected))
+        end
+      end
+    end
+  end
+
+  describe 'GET /accounts/{account_id}/apps/evolution_apis/{evolution_api_id}/pair_qr_code' do
+    context 'when is unauthenticated user' do
+      it 'returns unauthorized' do
+        get "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api.id}/pair_qr_code"
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    context 'when is authenticated user' do
+      let!(:evolution_api_connecting) { create(:apps_evolution_api, :connecting, account: account) }
+      before do
+        sign_in(user)
+      end
+      context 'when evolution_api is connecting' do
+        it 'should show qrcode' do
+          get "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api_connecting.id}/pair_qr_code"
+          expect(response).to have_http_status(200)
+          expect(response.body).to include('qrcode_connecting')
+        end
+      end
+      context 'when evolution_api is disconnected' do
+        it 'should show qrcode refresh link' do
+          get "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api.id}/pair_qr_code"
+          expect(response).to have_http_status(200)
+          expect(response.body).to include('Click here to refresh qr code')
+        end
+      end
+      context 'when evolution_api is connected' do
+        let!(:evolution_api_connected) { create(:apps_evolution_api, :connected, account: account) }
+        it 'should not show qrcode' do
+          get "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api_connected.id}/pair_qr_code"
+          expect(response).to have_http_status(200)
+          expect(response.body).to include('<img src="" alt="" class="mx-auto lg:m-0">')
+        end
+      end
+    end
+  end
+
+  describe 'POST /accounts/{account_id}/apps/evolution_apis/{evolution_api_id}/refresh_qr_code' do
+    let(:create_instance_response) { File.read('spec/integration/use_cases/accounts/apps/evolution_api/instance/create_response.json')}
+    before do
+      stub_request(:post, /instance/)
+        .to_return(body: create_instance_response, status: 201, headers: { 'Content-Type' => 'application/json' })
+    end
+    context 'when is unauthenticated user' do
+      it 'returns unauthorized' do
+        post "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api.id}/refresh_qr_code"
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    context 'when is authenticated user' do
+      before do
+        sign_in(user)
+      end
+      it do
+        expect do
+          post "/accounts/#{account.id}/apps/evolution_apis/#{evolution_api.id}/refresh_qr_code"
+        end.to change(Apps::EvolutionApi, :count).by(1)
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+end
