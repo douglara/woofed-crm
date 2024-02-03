b567d7d247823ad414919b1bc62372a2fd485537
Yukio Arie <yukioarie@gmail.com>
2024-02-03 03:53:09 -0300

    Separe broadcasts

--- app/models/apps/evolution_api.rb
+++ app/models/apps/evolution_api.rb
@@ -27,14 +27,13 @@
 class Apps::EvolutionApi < ApplicationRecord
   include Applicable
   include Rails.application.routes.url_helpers
+  include EvolutionApi::Broadcastable
 
   validates :endpoint_url, presence: true
   validates :token, presence: true
   validates :name, presence: true
   validates :instance, presence: true
   # validate :validate_evolution_api, on: :create
-  after_create :create_instance
-  after_commit :broadcast_update_qrcode, if: -> { saved_change_to_qrcode? }
   scope :actives, -> { where(active: true) }
 
   enum connection_status: {
@@ -44,29 +43,6 @@ class Apps::EvolutionApi < ApplicationRecord
     'connecting': 'connecting'
   }
 
-  after_update_commit do
-    if saved_change_to_connection_status?(from: 'inactive', to: 'active')
-        broadcast_replace_later_to "qrcode_#{self.id}", target: self, partial: '/components/redirect_page',
-      locals: { path: Rails.application.routes.url_helpers.account_apps_evolution_apis_path(self.account) }
-    end
-    broadcast_replace_later_to "evolution_apis_#{account_id}", target: self, partial: '/accounts/apps/evolution_apis/evolution_api',
-    locals: { evolution_api: self }
-  end
-
-  after_create_commit do
-    broadcast_append_later_to "evolution_apis_#{account_id}", target: 'evolution_apis', partial: '/accounts/apps/evolution_apis/evolution_api',
-    locals: { evolution_api: self }
-  end
-
-  after_destroy_commit do
-    broadcast_remove_to "evolution_apis_#{account_id}", target: self
-  end
-
-  def broadcast_update_qrcode
-    broadcast_replace_later_to "qrcode_#{self.id}", target: self, partial: 'accounts/apps/evolution_apis/qrcode',
-                                                 locals: { evolution_api: self }
-  end
-
   def request_instance_headers
     { 'apiKey': token.to_s, 'Content-Type': 'application/json' }
   end
@@ -75,15 +51,6 @@ class Apps::EvolutionApi < ApplicationRecord
     "#{ENV['FRONTEND_URL']}/apps/evolution_apis/webhooks"
   end
 
-  # def validate_evolution_api
-  #   result = Accounts::Apps::EvolutionApis::Instance::Create.call(self)
-  #   if result.key?(:error)
-  #     errors.add(:error, "#{result[:error]['response']['message']}")
-  #   end
-  # end
-  def create_instance
-    Accounts::Apps::EvolutionApis::Instance::Create.call(self)
-  end
 
   def generate_token(field)
     loop do
