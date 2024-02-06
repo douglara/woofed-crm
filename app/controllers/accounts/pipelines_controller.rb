f91a1e250474889e6ebb83e7f84ca7886a7b96f5
Yukio Arie <yukioarie@gmail.com>
2024-02-06 17:36:46 -0300

    Fix bulk_action if scheduled_at == nil and add beta flag

--- app/controllers/accounts/pipelines_controller.rb
+++ app/controllers/accounts/pipelines_controller.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'json_csv'
 
 class Accounts::PipelinesController < InternalController
-  before_action :set_pipeline, only: %i[ show edit update destroy ]
+  before_action :set_pipeline, only: %i[show edit update destroy]
 
   # GET /pipelines or /pipelines.json
   def index
@@ -17,11 +17,11 @@ class Accounts::PipelinesController < InternalController
   # GET /pipelines/1 or /pipelines/1.json
   def show
     @pipelines = current_user.account.pipelines
-    if params[:filter_status_deal].present?
-      @status = params[:filter_status_deal]
-    else
-      @status = 'open'
-    end
+    @status = if params[:filter_status_deal].present?
+                params[:filter_status_deal]
+              else
+                'open'
+              end
   end
 
   # GET /pipelines/new
@@ -45,31 +45,27 @@ class Accounts::PipelinesController < InternalController
 
     path_to_output_csv_file = "#{Rails.root}/tmp/deals-#{Time.current.to_i}.csv"
     line = 0
-    CSV.open(path_to_output_csv_file, "w") do |csv_output|
-
+    CSV.open(path_to_output_csv_file, 'w') do |csv_output|
       csv.each do |row|
-        if line == 0
-          csv_output << row.to_h.keys + ['result']
-        end
+        csv_output << row.to_h.keys + ['result'] if line == 0
 
         row_json = JsonCsv.csv_row_hash_to_hierarchical_json_hash(row, {})
 
-        row_params = ActionController::Parameters.new(row_json).merge({"stage_id": params[:stage_id]})
+        row_params = ActionController::Parameters.new(row_json).merge({ "stage_id": params[:stage_id] })
 
         deal = DealBuilder.new(current_user, row_params, true).perform
 
-        if deal.save
-          csv_output << row.to_h.values + ["Criado com sucesso id #{deal.id}"]
-        else
-          csv_output << row.to_h.values + ["Erro na criação #{deal.errors.messages}"]
-        end
+        csv_output << if deal.save
+                        row.to_h.values + ["Criado com sucesso id #{deal.id}"]
+                      else
+                        row.to_h.values + ["Erro na criação #{deal.errors.messages}"]
+                      end
         line += 1
       end
     end
 
-
     response.headers['Content-Type'] = 'text/csv'
-    response.headers['Content-Disposition'] = "attachment; filename=deals.csv"
+    response.headers['Content-Disposition'] = 'attachment; filename=deals.csv'
     # flash[:notice] = 'Arquivo processado com sucesso.'
     send_file path_to_output_csv_file
     # redirect_to account_pipeline_path(current_user.id, @pipeline.id), notice: 'Arquivo processado com sucesso.'
@@ -85,14 +81,14 @@ class Accounts::PipelinesController < InternalController
       format.html
       format.csv do
         path_to_output_csv_file = "#{Rails.root}/tmp/deals-#{Time.current.to_i}.csv"
-        #headers = Deal.csv_header(current_user.account)
-        headers = ["name", "contact_attributes.full_name", "contact_attributes.phone"]
-        CSV.open(path_to_output_csv_file, "w") do |csv|
+        # headers = Deal.csv_header(current_user.account)
+        headers = ['name', 'contact_attributes.full_name', 'contact_attributes.phone']
+        CSV.open(path_to_output_csv_file, 'w') do |csv|
           csv << headers
         end
 
         response.headers['Content-Type'] = 'text/csv'
-        response.headers['Content-Disposition'] = "attachment; filename=deals.csv"
+        response.headers['Content-Disposition'] = 'attachment; filename=deals.csv'
         render file: path_to_output_csv_file
       end
     end
@@ -104,18 +100,17 @@ class Accounts::PipelinesController < InternalController
 
     path_to_output_csv_file = "#{Rails.root}/tmp/deals-#{Time.current.to_i}.csv"
     JsonCsv.create_csv_for_json_records(path_to_output_csv_file) do |csv_builder|
-      @deals.each do | deal |
-        json = JSON.parse(deal.to_json(:include => :contacts))
+      @deals.each do |deal|
+        json = JSON.parse(deal.to_json(include: :contacts))
         csv_builder.add(json)
       end
     end
-
 
     respond_to do |format|
       format.html
       format.csv do
         response.headers['Content-Type'] = 'text/csv'
-        response.headers['Content-Disposition'] = "attachment; filename=deals.csv"
+        response.headers['Content-Disposition'] = 'attachment; filename=deals.csv'
         render file: path_to_output_csv_file
       end
     end
@@ -131,19 +126,20 @@ class Accounts::PipelinesController < InternalController
     @deals = current_user.account.deals.where(stage_id: params['stage_id'], status: 'open')
     if params['event']['send_now'] == 'true'
       time_start = DateTime.current
-    else
+    elsif !params['event']['scheduled_at'].nil?
       time_start = params['event']['scheduled_at'].to_time
     end
-    @result = @deals.each do | deal |
+    @result = @deals.each do |deal|
       if params['event']['kind'] == 'chatwoot_message'
         if params['event']['send_now'] == 'true'
-          time_start = time_start + rand(5..15).seconds
+          time_start += rand(5..15).seconds
           params['event']['send_now'] = 'false'
         elsif !time_start.nil?
-          time_start = time_start + rand(5..15).seconds
+          time_start += rand(5..15).seconds
         end
       end
-      @event = EventBuilder.new(current_user, event_params.merge({contact: deal.contact, scheduled_at: time_start})).build
+      @event = EventBuilder.new(current_user,
+                                event_params.merge({ contact: deal.contact, scheduled_at: time_start })).build
       @event.from_me = true
       @event.deal = deal
       @event.save
@@ -151,8 +147,7 @@ class Accounts::PipelinesController < InternalController
     redirect_to account_pipelines_path(current_user.account, @pipeline), notice: 'Envio em massa criado com sucesso!'
   end
 
-  def bulk_action_2
-  end
+  def bulk_action_2; end
 
   # POST /pipelines or /pipelines.json
   def create
@@ -160,7 +155,10 @@ class Accounts::PipelinesController < InternalController
 
     respond_to do |format|
       if @pipeline.save
-        format.html { redirect_to account_pipeline_path(current_user.account, @pipeline), notice: "Pipeline was successfully created." }
+        format.html do
+          redirect_to account_pipeline_path(current_user.account, @pipeline),
+                      notice: 'Pipeline was successfully created.'
+        end
         format.json { render :show, status: :created, location: @pipeline }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -173,7 +171,10 @@ class Accounts::PipelinesController < InternalController
   def update
     respond_to do |format|
       if @pipeline.update(pipeline_params)
-        format.html { redirect_to account_pipeline_path(current_user.account, @pipeline), notice: "Pipeline was successfully updated." }
+        format.html do
+          redirect_to account_pipeline_path(current_user.account, @pipeline),
+                      notice: 'Pipeline was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @pipeline }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -186,33 +187,35 @@ class Accounts::PipelinesController < InternalController
   def destroy
     @pipeline.destroy
     respond_to do |format|
-      format.html { redirect_to pipelines_url, notice: "Pipeline was successfully destroyed." }
+      format.html { redirect_to pipelines_url, notice: 'Pipeline was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pipeline
-      @pipeline = current_user.account.pipelines.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def pipeline_params
-      params.require(:pipeline).permit(:name, stages_attributes: [:id, :name, :_destroy, :account_id, :position])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pipeline
+    @pipeline = current_user.account.pipelines.find(params[:id])
+  end
 
-    def deal_params(params)
-      params.permit(
-        :name, :status, :stage_id, :contact_id,
-        contact_attributes: [ :id, :full_name, :phone, :email, :account_id ],
-        custom_attributes: {}
-      )
-    end
+  # Only allow a list of trusted parameters through.
+  def pipeline_params
+    params.require(:pipeline).permit(:name, stages_attributes: %i[id name _destroy account_id position])
+  end
 
-    def event_params
-      params.require(:event).permit(:content, :send_now, :done, :auto_done, :title, :kind, :app_type, :app_id, custom_attributes: {}, additional_attributes: {})
-    rescue
-      {}
-    end
+  def deal_params(params)
+    params.permit(
+      :name, :status, :stage_id, :contact_id,
+      contact_attributes: %i[id full_name phone email account_id],
+      custom_attributes: {}
+    )
+  end
+
+  def event_params
+    params.require(:event).permit(:content, :send_now, :done, :auto_done, :title, :kind, :app_type, :app_id,
+                                  custom_attributes: {}, additional_attributes: {})
+  rescue StandardError
+    {}
+  end
 end
