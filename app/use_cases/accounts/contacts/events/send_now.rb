b205bd21172eb50aaa47df6dc88a555dbf706812
Yukio Arie <yukioarie@gmail.com>
2024-02-15 16:56:16 -0300

    Fix message duplication

--- app/use_cases/accounts/contacts/events/send_now.rb
+++ app/use_cases/accounts/contacts/events/send_now.rb
@@ -2,7 +2,7 @@ class Accounts::Contacts::Events::SendNow
   def self.call(event)
     event.send_now = nil
     if event.chatwoot_message? || event.evolution_api_message?
-      event.update(scheduled_at: DateTime.current)
+      event.update(scheduled_at: DateTime.current, auto_done: false)
       if event.chatwoot_message?
         Accounts::Apps::Chatwoots::Messages::DeliveryJob.perform_later(event.id)
       elsif event.evolution_api_message?
