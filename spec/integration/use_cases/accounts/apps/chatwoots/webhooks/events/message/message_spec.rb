44327dbf0099a54594d59fdfcf4838396b230af1
Yukio Arie <yukioarie@gmail.com>
2024-02-23 22:14:54 -0300

    Add tests to messages with attachments

--- spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/message/message_spec.rb
+++ spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/message/message_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Accounts::Apps::Chatwoots::Webhooks::Events::Message, type: :requ
     let(:response_conversations) { File.read("spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/response_conversations.json") }
     let(:event_first) { Event.first}
 
-    context "receive event" do
+    context "receive event without attachment" do
       before do
         stub_request(:get, /contacts/).
         to_return(body: contact_response, status: 200, headers: {'Content-Type' => 'application/json'})
@@ -36,6 +36,39 @@ RSpec.describe Accounts::Apps::Chatwoots::Webhooks::Events::Message, type: :requ
         expect(event_first.content.body.to_plain_text).to eq('teste receive')
         expect(event_first.from_me).to eq(false)
         expect(event_first.additional_attributes).to include({'chatwoot_id' => 3750})
+      end
+    end
+    context 'receive event with attachment' do
+      before do
+        stub_request(:get, /contacts/).
+        to_return(body: contact_response, status: 200, headers: {'Content-Type' => 'application/json'})
+        stub_request(:get, /labels/).
+        to_return(body: {"payload": ["testc"]}.to_json, status: 200, headers: {'Content-Type' => 'application/json'})
+        stub_request(:get, /conversations/).
+        to_return(body: response_conversations, status: 200, headers: {'Content-Type' => 'application/json'})
+        stub_request(:any, /chatwoot\.server3\.woofedcrm\.com/).
+        to_return(body: 'file data')
+      end
+      context 'when receive event with one attachment' do
+        let(:event_message_with_one_attachment) { File. read("spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/message/event_message_with_one_attachment.json") }
+        it 'should crete one event with one attachment' do
+          expect do
+            Accounts::Apps::Chatwoots::Webhooks::Events::Message.call(chatwoot, JSON.parse(event_message_with_one_attachment))
+          end.to change(Event, :count).by(1)
+          expect(event_first.additional_attributes).to include({'chatwoot_id' => 8633})
+          expect(Attachment.count).to eq(1)
+        end
+      end
+      context 'when receive event with more than one attachment' do
+        let(:event_message_with_three_attachments) { File. read("spec/integration/use_cases/accounts/apps/chatwoots/webhooks/events/message/event_message_with_three_attachments.json") }
+        it 'should create 3 events and 3 attachments' do
+          expect do
+            Accounts::Apps::Chatwoots::Webhooks::Events::Message.call(chatwoot, JSON.parse(event_message_with_three_attachments))
+          end.to change(Event, :count).by(3)
+          expect(Event.last.content.to_plain_text).to eq("Message with attachments")
+          expect(Event.all.map(&:additional_attributes)).to include({'chatwoot_id' => 8637}).exactly(Event.count).times
+          expect(Attachment.count).to eq(3)
+        end
       end
     end
   end
