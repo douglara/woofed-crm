90950a433d1562e2fb36ab3c1fb7208238ea3b7e
Yukio Arie <yukioarie@gmail.com>
2024-02-29 23:42:01 -0300

    Refactor - remove duplication

--- app/use_cases/accounts/apps/chatwoots/webhooks/import_message.rb
+++ app/use_cases/accounts/apps/chatwoots/webhooks/import_message.rb
@@ -43,15 +43,6 @@ class Accounts::Apps::Chatwoots::Webhooks::ImportMessage
     message
   end
 
-  def self.import_attachment(event, attachment_params)
-    downloaded_file = URI.open(attachment_params['data_url'])
-    attachment = event.build_attachment(
-      file_type: attachment_params['file_type']
-    )
-    attachment.file.attach(io: downloaded_file,
-                           filename: "attachment_#{attachment_params['file_type']}_#{event.additional_attributes['chatwoot_id']}")
-  end
-
   def self.has_multiple_attachments?(webhook)
     webhook['attachments'].present? && webhook['attachments'].count > 1
   end
