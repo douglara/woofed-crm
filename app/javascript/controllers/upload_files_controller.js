d7e6141ee2b064532142d8074397004d736da38c
Yukio Arie <yukioarie@gmail.com>
2024-03-26 03:15:08 -0300

    Add progress bar to files upload and refactor

--- app/javascript/controllers/upload_files_controller.js
+++ app/javascript/controllers/upload_files_controller.js
@@ -5,10 +5,8 @@ export default class extends Controller {
   static values = {
     xmarkSvgUrl: String,
   };
-  static targets = ["fileInput", "preview"];
-  connect() {
-    console.log(this.xmarkSvgUrlValue);
-  }
+  static targets = ["fileInput"];
+
   trigger(event) {
     event.stopPropagation();
     this.fileInputTarget.click();
@@ -19,117 +17,101 @@ export default class extends Controller {
       ? event.dataTransfer.files
       : event.target.files;
     [...files].forEach((file) => {
-      this.uploadFile(file);
+      new Upload(file, this.xmarkSvgUrlValue, this.fileInputTarget).process();
+      // this.uploadFile(file);
     });
   }
   removeFile(event) {
     const divToDelete = event.target.closest('[id^="upload"]');
     divToDelete.remove();
   }
-
-  insertAfter(el, referenceNode) {
-    return referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+}
+class Upload {
+  constructor(file, xmarkSvgUrl, fileInput) {
+    this.directUpload = new DirectUpload(
+      file,
+      "/rails/active_storage/direct_uploads",
+      this
+    );
+    this.xmarkSvgUrl = xmarkSvgUrl;
+    this.fileInput = fileInput;
   }
 
-  // createHiddenInput() {
-  //   const input = document.createElement("input");
-  //   input.type = "hidden";
-  //   input.name = this.fileInputTarget.name;
-  //   // input.value = blob.signed_id
-  //   this.insertAfter(input, this.fileInputTarget);
-  //   return input;
-  // }
+  process() {
+    const fileWrapper = this.insertUpload();
+    this.directUpload.create((error, blob) => {
+      if (error) {
+        console.log(error);
+        fileWrapper.remove();
+      } else {
+        this.createHiddenBlobInput(blob, this.directUpload.id);
+      }
+    });
+  }
+
+  directUploadWillStoreFileWithXHR(request) {
+    request.upload.addEventListener("progress", (event) =>
+      this.updateProgress(event)
+    );
+  }
+  updateProgress(event) {
+    const percentage = (event.loaded / event.total) * 100;
+    const progress = document.querySelector(
+      `#upload_${this.directUpload.id} .progress-wrapper .progress-bar`
+    );
+    progress.style.width = `${percentage}%`;
+  }
   createHiddenBlobInput(blob, uploadId) {
     const input = document.createElement("input");
     const inputWrapper = document.getElementById(`upload_${uploadId}`);
     input.type = "hidden";
-    input.name = this.fileInputTarget.name;
+    input.name = this.fileInput.name;
     input.value = blob.signed_id;
-
     inputWrapper.appendChild(input);
     return input;
   }
-
-  // insertUpload(upload) {
-  //   const fileUpload = document.createElement("div");
-  //   fileUpload.textContent = upload.file.name;
-
-  //   const uploadList = document.querySelector("#uploads");
-  //   uploadList.appendChild(fileUpload);
-  // }
-
-  insertUpload(upload) {
+  insertUpload() {
     const fileWrapper = document.createElement("div");
     const fileName = document.createElement("p");
     const fileInfoWrapper = document.createElement("div");
     const fileThumb = document.createElement("img");
     const linkThumb = document.createElement("a");
-    const iconDelete = `<div class="p-2.5 cursor-pointer" data-action="click->upload-files#removeFile"><img src="${this.xmarkSvgUrlValue}" alt="" class="w-4" ></div>`;
-    // test.setAttribute("src", this.xmarkSvgUrlValue);
-    // const iconDelete = `<i data-lucide="x"></i>`;
-    fileInfoWrapper.setAttribute("data-controller", "lightbox");
-    // deleteFile.setAttribute("data-action", "click->upload-files#removeFile");
-    // deleteFile.className = "stroke-brand-palette-03";
-    // deleteFile.setAttribute("data-lucide", "x");
+    const iconDelete = `<div class="p-2.5 cursor-pointer" data-action="click->upload-files#removeFile"><img src="${this.xmarkSvgUrl}" alt="" class="w-4" ></div>`;
+    const progress = `<div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 flex-1 progress-wrapper">
+    <div class="bg-brand-palette-03 h-2.5 rounded-full progress-bar" style="width: 0%"></div>
+  </div>`;
     let reader = new FileReader();
+    reader.readAsDataURL(this.directUpload.file);
     reader.onloadend = function () {
-      fileThumb.src = reader.result;
-      linkThumb.href = reader.result;
+      if (reader.result !== null) {
+        fileInfoWrapper.setAttribute("data-controller", "lightbox");
+        fileThumb.src = reader.result;
+        linkThumb.href = reader.result;
+      } else {
+        fileName.textContent = "Error";
+      }
     };
-    reader.readAsDataURL(upload.file);
 
-    fileWrapper.id = `upload_${upload.id}`;
+    fileWrapper.id = `upload_${this.directUpload.id}`;
     fileWrapper.className =
-      "p-1 pr-2 border border-light-palette-p3 rounded-lg flex items-center justify-between";
+      "p-1 pr-2 border border-light-palette-p3 rounded-lg flex items-center gap-10";
     fileName.className =
-      "text-dark-gray-palette-p1 typography-text-m-lh150 max-w-[300px] truncate";
+      "text-dark-gray-palette-p1 typography-text-m-lh150 w-56 truncate";
     fileThumb.className = "w-10 h-10 rounded-lg object-cover";
     linkThumb.className = "flex gap-4 items-center";
-    // deleteFile.className = "stroke-brand-palette-03";
-    // deleteFile.textContent = "Remove";
+    fileName.textContent = this.directUpload.file.name;
     linkThumb.appendChild(fileThumb);
     linkThumb.appendChild(fileName);
     fileInfoWrapper.appendChild(linkThumb);
-    // deleteFile.className =
-    //   "typography-text-r-lh150 cursor-pointer text-dark-gray-palette-p3 hover:text-dark-gray-palette-p2";
-
-    fileName.textContent = upload.file.name;
     fileWrapper.appendChild(fileInfoWrapper);
-    // deleteFile.insertAdjacentHTML("afterbegin", deleteDescription);
-    // deleteFile.appendChild(iconDelete);
+    fileWrapper.insertAdjacentHTML("beforeend", progress);
     fileWrapper.insertAdjacentHTML("beforeend", iconDelete);
-    // fileWrapper.appendChild(test);
+    this.addFileToUploadList(fileWrapper);
 
-    // const progressWrapper = document.createElement("div");
-    // progressWrapper.className =
-    //   "relative h-4 overflow-hidden rounded-full bg-secondary w-[100%]";
-    // fileUpload.appendChild(progressWrapper);
-
-    // const progressBar = document.createElement("div");
-    // progressBar.className = "progress h-full w-full flex-1 bg-primary";
-    // progressBar.style = "transform: translateX(-100%);";
-    // progressWrapper.appendChild(progressBar);
-
-    const uploadList = document.querySelector("#uploads");
-    uploadList.appendChild(fileWrapper);
+    return fileWrapper;
   }
-  uploadFile(file) {
-    // let hiddenInput = this.createHiddenInput();
-    const upload = new DirectUpload(
-      file,
-      "/rails/active_storage/direct_uploads"
-    );
-    const uploadId = upload.id;
-
-    this.insertUpload(upload);
-    upload.create((error, blob) => {
-      if (error) {
-        // Handle the error
-      } else {
-        // debugger;
-        this.createHiddenBlobInput(blob, uploadId);
-        // hiddenInput.value = blob.signed_id;
-      }
-    });
+  addFileToUploadList(file) {
+    const uploadList = document.querySelector("#uploads");
+    uploadList.appendChild(file);
   }
 }
