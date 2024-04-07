cd1b4c9f35d48394e081bc48be1af7fdd9430938
Yukio Arie <yukioarie@gmail.com>
2024-04-07 17:13:44 -0300

    Improve sidebar expand system

--- app/javascript/controllers/element_expanded_controller.js
+++ app/javascript/controllers/element_expanded_controller.js
@@ -1,9 +1,28 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
+import Rails from "@rails/ujs";
 
 export default class extends Controller {
+  static targets = ["sidebar"];
   toggle() {
-    var element = document.getElementById("element-expand")
-    var expanded = (element.ariaExpanded === 'true');
-    element.ariaExpanded = !expanded;    
+    var element = document.getElementById("element-expand");
+    var expanded = element.ariaExpanded === "true";
+    element.ariaExpanded = !expanded;
+
+    if (this.hasSidebarTarget) {
+      const currentUrl = window.location.href;
+      const url = new URL(currentUrl);
+      url.searchParams.set("sidebar_expanded", !expanded);
+      console.log(url);
+      this.updateSidebarExpanded(!expanded);
+    }
+  }
+  async updateSidebarExpanded(expanded) {
+    const currentUrl = window.location.href;
+    const url = new URL(currentUrl);
+    url.searchParams.set("sidebar_expanded", expanded);
+    Rails.ajax({
+      url: url,
+      type: "GET",
+    });
   }
 }
