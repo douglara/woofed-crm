a053bad5c204171ecc517f6883a4e5a4ed3c8712
Yukio <45243355+YukioArie@users.noreply.github.com>
2024-04-10 07:38:11 -0300

    Improve sidebar expand system (#283)
    * Improve sidebar expand system
    
    * Replace cookies for localStorage to store sidebar_expanded value
    
    * Remove ujs rails import

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
-
   include Pagy::Backend
   if ENV['HIGHLIGHT_PROJECT_ID'].present?
-    require "highlight"
+    require 'highlight'
     include Highlight::Integrations::Rails
     around_action :with_highlight_context
   end
