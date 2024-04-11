0cb42f495c8d842181a39a3e5a4d9812522513d0
Yukio <45243355+YukioArie@users.noreply.github.com>
2024-04-11 09:46:40 -0300

    Add pagination to events using turbo frames (#282)
    * Add pagination to events_to_do
    
    * Add pagination to events done
    
    * Fix items per page
    
    * Add pagination turbo frame to event_to_do
    
    * Add pagination turbo_frame to events_done and fix turbo_frame pagination system
    
    * Add max height to events_to_do window
    
    * Add test events page to deals controller
    
    * Change ids from pagination turboframe
    
    * Update pagination events tests

--- app/models/concerns/deal/broadcastable.rb
+++ app/models/concerns/deal/broadcastable.rb
@@ -1,35 +1,38 @@
 module Deal::Broadcastable
   extend ActiveSupport::Concern
   included do
-
-    after_create_commit {
-      if self.done == false
+    after_create_commit do
+      if done == false
         broadcast_prepend_later_to [contact_id, 'events'],
-        partial: "accounts/contacts/events/event",
-        target: "events_to_do_#{contact.id}"
+                                   partial: 'accounts/contacts/events/event',
+                                   target: "events_to_do_#{contact.id}"
       else
         broadcast_prepend_later_to [contact_id, 'events'],
-        partial: "accounts/contacts/events/event",
-        target: "events_done_#{contact.id}"
+                                   partial: 'accounts/contacts/events/event',
+                                   target: "events_done_#{contact.id}"
       end
-    }
-
-    def broadcast_events
-      broadcast_replace_later_to [contact_id, 'events'], target: "events_to_do_#{contact.id}", partial: 'accounts/contacts/events/events_to_do', locals: {deal: deal}
-      broadcast_replace_later_to [contact_id, 'events'], target: "events_done_#{contact.id}", partial: 'accounts/contacts/events/events_done', locals: {deal: deal}
     end
 
-    after_update_commit {
+    def broadcast_events
+      events_to_do = deal.contact.events.to_do.limit(5).to_a
+      events_done = deal.contact.events.done.limit(5).to_a
+      broadcast_replace_later_to [contact_id, 'events'], target: "events_to_do_#{contact.id}",
+                                                         partial: 'accounts/contacts/events/events_to_do', locals: { deal: deal, events: events_to_do, pagy: 1 }
+      broadcast_replace_later_to [contact_id, 'events'], target: "events_done_#{contact.id}",
+                                                         partial: 'accounts/contacts/events/events_done', locals: { deal: deal, events: events_done, pagy: 1 }
+    end
+
+    after_update_commit do
       if saved_change_to_done_at?
-        broadcast_events()
+        broadcast_events
       else
         broadcast_replace_later_to [contact_id, 'events'],
-        partial: "accounts/contacts/events/event"
+                                   partial: 'accounts/contacts/events/event'
       end
-    }
+    end
 
-    after_destroy_commit {
+    after_destroy_commit do
       broadcast_remove_to [contact_id, 'events']
-    }
+    end
   end
 end
