acebfe0873f85a92f76f950a0298c0a4fcd44962
Yukio <45243355+YukioArie@users.noreply.github.com>
2024-04-16 00:44:22 -0300

    Add record attachment frontend (#293)

--- app/javascript/controllers/upload_files_controller.js
+++ app/javascript/controllers/upload_files_controller.js
@@ -1,16 +1,52 @@
 import { Controller } from "stimulus";
 import { DirectUpload } from "@rails/activestorage";
+import WaveSurfer from "wavesurfer.js";
+import RecordPlugin from "wavesurfer.js/dist/plugins/record.esm.js";
+import Hover from "wavesurfer.js/dist/plugins/hover.js";
 
 export default class extends Controller {
-  static targets = ["fileInput", "dragAlert"];
+  static targets = [
+    "fileInput",
+    "dragAlert",
+    "micWave",
+    "recordIcon",
+    "micWaveWrapper",
+    "progressRecord",
+  ];
+  static values = {
+    pauseRecordSvgUrl: String,
+    micSvgUrl: String,
+    pauseWaveSvgUrl: String,
+    playWaveSvgUrl: String,
+  };
 
   connect() {
+    this.configWaveRecord();
     this.eventListenerDragAndDrop("add");
   }
 
   disconnect() {
     this.eventListenerDragAndDrop("remove");
   }
+
+  recordAudio() {
+    if (this.record.isRecording() || this.record.isPaused()) {
+      this.record.stopRecording();
+      this.micWaveWrapperTarget.classList.add("hidden");
+      this.recordIconTarget.src = this.micSvgUrlValue;
+      return;
+    }
+    let recordDevice;
+
+    RecordPlugin.getAvailableAudioDevices().then((devices) => {
+      recordDevice = devices[0].deviceId;
+    });
+    this.record.startRecording({ recordDevice }).then(() => {
+      this.micWaveWrapperTarget.classList.remove("hidden");
+      this.recordIconTarget.src = this.pauseRecordSvgUrlValue;
+    });
+  }
+
   eventListenerDragAndDrop(state) {
     if (state === "add") {
       this.element.addEventListener("dragover", this.preventDragDefaults);
@@ -46,27 +82,81 @@ export default class extends Controller {
       ? event.dataTransfer.files
       : event.target.files;
     [...files].forEach((file) => {
-      new Upload(file, this.fileInputTarget).process();
+      new Upload(
+        file,
+        this.fileInputTarget,
+        this.pauseWaveSvgUrlValue,
+        this.playWaveSvgUrlValue
+      ).process();
     });
   }
   removeFile(event) {
     const divToDelete = event.target.closest('[id^="upload"]');
     divToDelete.remove();
   }
+
+  configWaveRecord() {
+    const wavesurfer = WaveSurfer.create({
+      container: this.micWaveTarget,
+      waveColor: "#D9DEFF",
+      progressColor: "#6756D6",
+      height: 37,
+      barHeight: 4,
+    });
+    this.record = wavesurfer.registerPlugin(
+      RecordPlugin.create({
+        scrollingWaveform: false,
+        renderRecordedAudio: false,
+      })
+    );
+    this.bindWaveRecordEvents();
+  }
+  bindWaveRecordEvents() {
+    const updateProgress = (time) => {
+      const formattedTime = [
+        Math.floor((time % 3600000) / 60000),
+        Math.floor((time % 60000) / 1000),
+      ]
+        .map((v) => (v < 10 ? "0" + v : v))
+        .join(":");
+      this.progressRecordTarget.textContent = formattedTime;
+    };
+    this.record.on("record-progress", (time) => {
+      updateProgress(time);
+    });
+
+    this.record.on("record-end", (blob) => {
+      const timestamp = new Date().getTime();
+      const fileName = `${timestamp}.oga`;
+      const audioFile = new File([blob], fileName, {
+        type: "audio/ogg",
+      });
+
+      new Upload(
+        audioFile,
+        this.fileInputTarget,
+        this.pauseWaveSvgUrlValue,
+        this.playWaveSvgUrlValue
+      ).process();
+    });
+  }
 }
 class Upload {
-  constructor(file, fileInput) {
+  constructor(file, fileInput, pauseSvg, playSvg) {
     this.directUpload = new DirectUpload(
       file,
       "/rails/active_storage/direct_uploads",
       this
     );
     this.fileInput = fileInput;
+    this.pauseSvg = pauseSvg;
+    this.playSvg = playSvg;
   }
 
   process() {
     const fileWrapper = this.insertUpload();
     const progressBar = fileWrapper.querySelector("#progressWrapper");
+
     if (this.isFileSizeExceeded()) {
       progressBar.remove();
       this.showErrorMessage(
@@ -79,10 +169,71 @@ class Upload {
         if (error) {
           this.showErrorMessage(error, fileWrapper);
         } else {
+          if (this.fileTypeIs("audio")) {
+            this.createWaveForm(fileWrapper);
+          }
           this.createHiddenBlobInput(blob, this.directUpload.id);
         }
       });
     }
+  }
+  createWaveForm(fileWrapper) {
+    const uploadInfo = fileWrapper.querySelector(
+      `#upload_${this.directUpload.id}_info`
+    );
+    const waveWrapper = document.createElement("div");
+    const wave = document.createElement("div");
+    const audioDuration = document.createElement("p");
+    waveWrapper.className = "flex items-center gap-2.5 justify-end";
+    audioDuration.className =
+      "typography-body-m-lh150 text-dark-gray-palette-p2";
+    waveWrapper.appendChild(wave);
+    waveWrapper.appendChild(audioDuration);
+    uploadInfo.appendChild(waveWrapper);
+
+    const reader = new FileReader();
+    reader.readAsDataURL(this.directUpload.file);
+    reader.onloadend = () => {
+      const audioUrl = reader.result;
+      const waveForm = WaveSurfer.create({
+        container: wave,
+        waveColor: "#D9DEFF",
+        progressColor: "#6756D6",
+        url: audioUrl,
+        height: 42,
+        barGap: 4,
+        barRadius: 30,
+        barWidth: 2,
+        width: 412,
+        cursorWidth: 0,
+        plugins: [Hover.create()],
+      });
+      waveForm.on("finish", () => {
+        playPauseBtn.querySelector("img").setAttribute("src", this.playSvg);
+      });
+      waveForm.on("ready", () => {
+        audioDuration.textContent = this.formatTime(waveForm.getDuration());
+      });
+      const playPauseBtn = fileWrapper.querySelector("#playPauseBtn");
+      playPauseBtn.classList.remove("pointer-events-none");
+      playPauseBtn.onclick = (event) => {
+        event.preventDefault();
+        waveForm.playPause();
+        this.togglePlayIcon(playPauseBtn.querySelector("img"));
+      };
+    };
+  }
+  formatTime(time) {
+    return [
+      Math.floor((time % 3600) / 60), // minutes
+      ("00" + Math.floor(time % 60)).slice(-2), // seconds
+    ].join(":");
+  }
+  togglePlayIcon(img) {
+    const newIconSrc = img.getAttribute("src").includes(this.playSvg)
+      ? this.pauseSvg
+      : this.playSvg;
+    img.setAttribute("src", newIconSrc);
   }
   isFileSizeExceeded() {
     const fileSize = this.directUpload.file.size;
@@ -109,8 +260,28 @@ class Upload {
     input.value = blob.signed_id;
     inputWrapper.appendChild(input);
   }
+  createAudioWrapper() {
+    const fileWrapper = document.querySelector("#audioWrapper").cloneNode(true);
+    const uploadInfo = fileWrapper.querySelector("#uploadInfo");
+    const fileName = fileWrapper.querySelector("#fileName");
+    fileWrapper.classList.remove("hidden");
+    fileWrapper.id = `upload_${this.directUpload.id}`;
+    uploadInfo.id = `upload_${this.directUpload.id}_info`;
+    fileName.textContent = this.directUpload.file.name;
+    this.addFileToUploadList(fileWrapper);
+    return fileWrapper;
+  }
   insertUpload() {
-    const fileWrapper = document.querySelector("#fileWrapper").cloneNode(true);
+    let fileWrapper;
+    if (this.fileTypeIs("audio")) {
+      fileWrapper = this.createAudioWrapper();
+    } else {
+      fileWrapper = this.createFileWrapper();
+    }
+    return fileWrapper;
+  }
+  createFileWrapper() {
+    document.querySelector("#fileWrapper").cloneNode(true);
     const uploadInfo = fileWrapper.querySelector("#uploadInfo");
     const fileName = fileWrapper.querySelector("#fileName");
     fileWrapper.classList.remove("hidden");
