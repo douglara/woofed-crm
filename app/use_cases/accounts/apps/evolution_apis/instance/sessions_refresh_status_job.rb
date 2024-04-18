f7d5c914cc2ec6779a13acd6633669bf2dc6bce1
Douglas Lara <douglasdelaras@gmail.com>
2024-04-17 21:00:21 -0300

    Evolution API status refresh

---
+++ app/use_cases/accounts/apps/evolution_apis/instance/sessions_refresh_status_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Accounts::Apps::EvolutionApis::Instance::SessionsRefreshStatusJob < ApplicationJob
+  self.queue_adapter = :good_job
+
+  def perform
+    Apps::EvolutionApi.connected.find_each do |evolution_api|
+      Accounts::Apps::EvolutionApis::Instance::RefreshStatus.new(evolution_api).call
+    end
+  end
+end
---
+++ spec/integration/use_cases/accounts/apps/evolution_api/instance/refresh_status_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Accounts::Apps::EvolutionApis::Instance::RefreshStatus, type: :request do
+  describe '.call' do
+    let(:account) { create(:account) }
+    let(:evolution_api) { create(:apps_evolution_api, :connected, account: account) }
+
+    it 'when instance does not exist' do
+      stub_request(:get, /connectionState/)
+        .to_return(body: '{"status":404,"error":"Not Found","response":{"message":["The \"9244ecdbda2c251315e6\" instance does not exist"]}}', status: 404, headers: { 'Content-Type' => 'application/json' })
+      described_class.new(evolution_api).call
+      expect(evolution_api.connection_status).to eq('disconnected')
+    end
+
+    it 'when instance is connected' do
+      stub_request(:get, /connectionState/)
+        .to_return(body: '{"instance":{"instanceName":"dd7de5acfca04cd65459","state":"open"}}', status: 200, headers: { 'Content-Type' => 'application/json' })
+      described_class.new(evolution_api).call
+      expect(evolution_api.connection_status).to eq('connected')
+    end
+  end
+end
