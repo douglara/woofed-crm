864bb618d0a9f388bba4876c5623aef0375f3dcd
Yukio Arie <yukioarie@gmail.com>
2024-04-21 23:25:50 -0300

    Fix custom attributes exhibition

--- app/models/concerns/custom_attributes.rb
+++ app/models/concerns/custom_attributes.rb
@@ -3,8 +3,10 @@ module CustomAttributes
 
   def custom_attribute_display_name(attribute_key)
     account.custom_attribute_definitions.where(
-      attribute_model: "#{this.class.name.downcase}_attribute", 
+      attribute_model: "#{self.class.name.downcase}_attribute",
       attribute_key: attribute_key
-    ).first.attribute_display_name rescue attribute_key
+    ).first.attribute_display_name
+  rescue StandardError
+    attribute_key
   end
 end
