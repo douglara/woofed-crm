357794dfb527350b81d8b0275a4806c7c997ed88
Yukio Arie <yukioarie@gmail.com>
2024-04-24 20:46:04 -0300

    Add mask to amount_in_cents field

---
+++ app/javascript/controllers/input_mask/input_amount_in_cents_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "stimulus";
+import IMask from "imask";
+
+export default class extends Controller {
+  connect() {
+    new IMask(this.element, {
+      mask: "num",
+      blocks: {
+        num: {
+          mask: Number,
+          scale: 2,
+          thousandsSeparator: ".",
+          padFractionalZeros: true,
+          normalizeZeros: true,
+          radix: ",",
+        },
+      },
+    });
+  }
+}
