2e52f72f064fae1f4b26d22291beee42d2bc5a75
Douglas Lara <douglasdelaras@gmail.com>
2024-04-25 19:04:39 -0300

    Migrate abstract methods to instance methods

--- app/use_cases/accounts/apps/chatwoots/sync_import_contacts.rb
+++ app/use_cases/accounts/apps/chatwoots/sync_import_contacts.rb
@@ -1,10 +1,16 @@
 class Accounts::Apps::Chatwoots::SyncImportContacts
-  def self.call(chatwoot)
-    response = update_or_create_contact(chatwoot)
+
+  def initialize(chatwoot)
+    @chatwoot = chatwoot
+    @account = @chatwoot.account
+  end
+
+  def call
+    response = update_or_create_contact
     { ok: response }
   end
-    
-  def self.update_or_create_contact(chatwoot)
+
+  def update_or_create_contact
     contacts_imported = 0
     contacts_failed = 0
     contacts_updated = 0
@@ -13,30 +19,30 @@ class Accounts::Apps::Chatwoots::SyncImportContacts
     until quantity_per_page.zero?
       page += 1
       request = Faraday.get(
-        "#{chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot.chatwoot_account_id}/contacts/",
+        "#{@chatwoot.chatwoot_endpoint_url}/api/v1/accounts/#{@chatwoot.chatwoot_account_id}/contacts/",
         { page: page },
-        chatwoot.request_headers
+        @chatwoot.request_headers
       )
       if request.status == 200
         chatwoot_contacts = JSON.parse(request.body)['payload']
         quantity_per_page = chatwoot_contacts.count
         chatwoot_contacts.each do |chatwoot_contact|
-          contact = Accounts::Contacts::GetByParams.call(Account.first,
+          contact = Accounts::Contacts::GetByParams.call(@account,
                                                          chatwoot_contact.slice('email',
                                                                                 'phone').transform_values(&:to_s))
           if contact[:ok]
             update_contact_chatwoot_id(contact[:ok], chatwoot_contact['id'])
-            import_labels(chatwoot, contact[:ok])
+            import_labels(contact[:ok])
             contact[:ok].save
             contacts_updated += 1
           else
-            contact = build_contact_att(chatwoot_contact, chatwoot)
-            import_labels(chatwoot, contact)
+            contact = build_contact_att(chatwoot_contact)
+            import_labels(contact)
             if contact.save
               contacts_imported += 1
             else
               contacts_failed += 1
-              Rails.logger.error("Error import contact from chatwoot #{contact.erros.inspect}, chatwoot: #{chatwoot.inspect}")
+              Rails.logger.error("Error import contact from chatwoot #{contact.erros.inspect}, chatwoot: #{@chatwoot.inspect}")
             end
           end
         end
@@ -47,17 +53,17 @@ class Accounts::Apps::Chatwoots::SyncImportContacts
     "Contacts imported #{contacts_imported} / Contacts updated #{contacts_updated} / Contacts failed #{contacts_failed}"
   end
 
-  def self.update_contact_chatwoot_id(contact, chatwoot_id)
+  def update_contact_chatwoot_id(contact, chatwoot_id)
     contact.assign_attributes(additional_attributes: { chatwoot_id: chatwoot_id })
   end
 
-  def self.import_labels(chatwoot, contact)
-    Accounts::Apps::Chatwoots::Webhooks::ImportContact.import_contact_tags(chatwoot, contact)
-    Accounts::Apps::Chatwoots::Webhooks::ImportContact.import_contact_converstions_tags(chatwoot, contact)
+  def import_labels(contact)
+    Accounts::Apps::Chatwoots::Webhooks::ImportContact.import_contact_tags(@chatwoot, contact)
+    Accounts::Apps::Chatwoots::Webhooks::ImportContact.import_contact_converstions_tags(@chatwoot, contact)
   end
 
-  def self.build_contact_att(body, chatwoot)
-    contact = chatwoot.account.contacts.new(
+  def build_contact_att(body)
+    contact = @account.contacts.new(
       full_name: body['name'],
       email: (body['email']).to_s,
       phone: (body['phone_number']).to_s
