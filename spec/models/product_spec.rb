590d3aa3681488935c7fb821853a346c82f8a2fe
Yukio Arie <yukioarie@gmail.com>
2024-04-27 03:09:46 -0300

    Add individual test to amount_in_cents

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,6 +24,45 @@
 #
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Product do
+  let!(:account) { create(:account) }
+  let(:product_first) { Product.first }
+  describe 'create a product' do
+    let(:new_product) { Product.new }
+    context 'check amount_in_cents' do
+      context 'when amount_in_cents is string' do
+        it 'should convert amount_in_cents to integer and  create a product' do
+          expect do
+            product = new_product
+            product.amount_in_cents = '100.580,30'
+            product.account = account
+            product.save
+          end.to change(Product, :count).by(1)
+          expect(product_first.amount_in_cents).to eq(10_058_030)
+        end
+        context 'when amount_in_cents is negative' do
+          it 'should not create a product and return error' do
+            expect do
+              product = new_product
+              product.amount_in_cents = '-100.580,30'
+              product.account = account
+              product.save
+              expect(product.errors[:amount_in_cents]).to include('Can not be negative')
+            end.to change(Product, :count).by(0)
+          end
+        end
+      end
+      context 'when amount_in_cents is integer' do
+        it 'should create a product' do
+          expect do
+            product = new_product
+            product.amount_in_cents = 10_058_030
+            product.account = account
+            product.save
+          end.to change(Product, :count).by(1)
+          expect(product_first.amount_in_cents).to eq(10_058_030)
+        end
+      end
+    end
+  end
 end
