cfa7e54d7c94995960f15ff5297e03309ea45425
Douglas Lara <douglasdelaras@gmail.com>
2024-05-02 02:06:51 -0300

    Add UI settings

---
+++ app/controllers/accounts/settings/ai_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Accounts::Settings::AiController < InternalController
+  before_action :set_account
+
+  def edit; end
+
+  def update
+    if @account.update(account_params)
+      redirect_to edit_account_ai_path(current_user.account, current_user.account), notice: 'Configuration updated successfully'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_account
+    @account = current_user.account
+  end
+
+  def account_params
+    params.require(:account).permit(:woofbot_auto_reply)
+  end
+end
