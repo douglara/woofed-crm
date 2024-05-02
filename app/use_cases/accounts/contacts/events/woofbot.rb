6e215fb2d9df0b655a4505ad520f914769105f6f
Douglas Lara <douglasdelaras@gmail.com>
2024-05-02 02:50:35 -0300

    Improve woofot code

--- app/use_cases/accounts/contacts/events/woofbot.rb
+++ app/use_cases/accounts/contacts/events/woofbot.rb
@@ -7,30 +7,38 @@ class Accounts::Contacts::Events::Woofbot
   end
 
   def call
-    if @event.from_me == false && woofbot_active? && @event.content.to_s.include?('?')
-      woofbot_response = Accounts::Contacts::Events::GenerateAiResponse.new(@event).call
+    return unless woofbot_should_be_run?
 
-      if woofbot_response.present?
+    @woofbot_response = Accounts::Contacts::Events::GenerateAiResponse.new(@event).call
+    create_reply_event if @woofbot_response.present?
+  end
 
-        event_params = {
-          kind: @event.kind,
-          contact_id: @event.contact_id,
-          app_type: @event.app_type,
-          app_id: @event.app_id,
-          from_me: true,
-          send_now: true,
-          content: woofbot_response
-        }
+  def create_reply_event
+    event_params = {
+      kind: @event.kind,
+      contact_id: @event.contact_id,
+      app_type: @event.app_type,
+      app_id: @event.app_id,
+      from_me: true,
+      send_now: true,
+      content: @woofbot_response
+    }
 
-        event_params.merge!({ deal_id: @event.deal_id }) if @event.deal_id.present?
+    event_params.merge!({ deal_id: @event.deal_id }) if @event.deal_id.present?
 
-        @response_event = EventBuilder.new(@account.users.first,
-                                           event_params).build
+    @response_event = EventBuilder.new(@account.users.first,
+                                        event_params).build
 
-        @response_event.save
-        @response_event
-      end
-    end
+    @response_event.save
+    @response_event
+  end
+
+  def woofbot_should_be_run?
+    @event.from_me == false && woofbot_active? && event_is_question?
+  end
+
+  def event_is_question?
+    @event.content.to_s.include?('?')
   end
 
   def woofbot_active?
