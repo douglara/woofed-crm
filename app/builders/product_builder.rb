005d504604b9b48729eed9b1ef6fb62383cec366
Yukio <45243355+YukioArie@users.noreply.github.com>
2024-05-11 21:34:19 -0300

    Add image product frontend (#314)
    * Add image product frontend
    
    * Refactor product partail image carousel exhibition
    
    * Fix width image exhibition
    
    * Change witdh image products list
    
    * Remove height partial and adjust sizes from product image

--- app/builders/product_builder.rb
+++ app/builders/product_builder.rb
@@ -6,7 +6,6 @@ class ProductBuilder
 
   def build
     @product = @user.account.products.new(@params)
-    build_files if @params.key?('files')
     @product
   end
 
@@ -14,18 +13,4 @@ class ProductBuilder
     build
     @product
   end
-
-  def build_files
-    @params[:files].each do |file|
-      config_attachment(file)
-    end
-  end
-
-  # def config_attachment(file)
-  #   attachment = @product.build_attachment
-  #   attachment.file = file
-  #   attachment.file_type = attachment.check_file_type
-  # rescue StandardError
-  #   @product.invalid_files = true
-  # end
 end
