55a09455c57b18def124d1f41af7b02df9d3b6a0
Yukio <45243355+YukioArie@users.noreply.github.com>
2024-05-13 07:46:24 -0300

    Fix cancel button events form (#285)
    * Fix cancel button events form
    
    * Update app/controllers/accounts/deals_controller.rb
    
    Co-authored-by: Douglas Lara <douglasdelaras@gmail.com>
    
    * Fix activity form
    
    * Fix cancel edit button
    
    * Add action show to use in cancel btn event form
    
    * Separe cancel button in partial
    
    ---------
    
    Co-authored-by: Douglas Lara <douglasdelaras@gmail.com>

--- app/controllers/accounts/deals_controller.rb
+++ app/controllers/accounts/deals_controller.rb
@@ -137,7 +137,6 @@ class Accounts::DealsController < InternalController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_deal
     @deal = current_user.account.deals.find(params[:id])
   end
