60630867f69020dd0015aa03aa367316a270210c
Yukio Arie <yukioarie@gmail.com>
2024-05-15 00:48:53 -0300

    Add i18n to application

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,7 +2,7 @@
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
 # Permitted locales available for the application
-I18n.available_locales = %i[en pt-BR]
+I18n.available_locales = %i[en pt-BR es]
 
 # Set default locale to something other than :en
-I18n.default_locale = :'pt-BR'
+I18n.default_locale = 'es'
