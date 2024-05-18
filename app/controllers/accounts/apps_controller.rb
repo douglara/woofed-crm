782d2d408244bea9944e007d3ae23f6fb3f0888c
Yukio Arie <yukioarie@gmail.com>
2024-05-18 02:38:17 -0300

    Add translations

--- app/controllers/accounts/apps_controller.rb
+++ app/controllers/accounts/apps_controller.rb
@@ -1,5 +1,5 @@
 class Accounts::AppsController < InternalController
-  before_action :set_contact, only: %i[ show edit update destroy ]
+  before_action :set_contact, only: %i[show edit update destroy]
 
   # GET /contacts or /contacts.json
   def index
@@ -13,15 +13,14 @@ class Accounts::AppsController < InternalController
   end
 
   # GET /contacts/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /contacts or /contacts.json
   def create
     @contact = current_user.account.contacts.new(contact_params)
 
     if @contact.save
-      redirect_to account_contact_path(current_user.account, @contact), notice: "Contact was successfully updated."
+      redirect_to account_contact_path(current_user.account, @contact), notice: 'Contact was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -31,7 +30,7 @@ class Accounts::AppsController < InternalController
   def update
     respond_to do |format|
       if @contact.update(contact_params)
-        redirect_to account_contact_path(current_user.account, @contact), notice: "Contact was successfully updated."
+        redirect_to account_contact_path(current_user.account, @contact), notice: 'Contact was successfully updated.'
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @contact.errors, status: :unprocessable_entity }
@@ -43,19 +42,20 @@ class Accounts::AppsController < InternalController
   def destroy
     @contact.destroy
     respond_to do |format|
-      format.html { redirect_to contacts_url, notice: "Contact was successfully destroyed." }
+      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def contact_params
-      params.require(:contact).permit(:full_name, :phone, :email, custom_attributes: {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def contact_params
+    params.require(:contact).permit(:full_name, :phone, :email, custom_attributes: {})
+  end
 end
--- spec/controllers/accounts/apps/chatwoots_controller_spec.rb
+++ spec/controllers/accounts/apps/chatwoots_controller_spec.rb
@@ -6,33 +6,31 @@ RSpec.describe Accounts::Apps::ChatwootsController, type: :request do
   let(:chatwoot) { create(:apps_chatwoots, :skip_validate, account: account) }
   let!(:user) { create(:user, account: account) }
   let(:app_chatwoot_created) { Apps::Chatwoot.first }
-  let(:dashboard_app_response) { File.read('spec/controllers/accounts/apps/dashboard_app_response.json')}
-  let(:webhooks_response) { File.read('spec/controllers/accounts/apps/webhooks_response.json')}
-  let(:inboxes_response) { File.read('spec/integration/use_cases/accounts/apps/chatwoots/inboxes.json')}
-  let(:valid_params){
-                      { apps_chatwoot: { 
-                          chatwoot_endpoint_url: 'https://chatwoot.test.com/',
-                          chatwoot_account_id: '2', 
-                          chatwoot_user_token: 'ASdasfdgfdgwEWWdfgfhgAWSDS'
-                        }
-                      }
-                    }
-  let(:invalid_params){
-    { apps_chatwoot: { 
-        chatwoot_endpoint_url: 'invalid_url',
-        chatwoot_account_id: '2', 
-        chatwoot_user_token: 'ASdasfdgfdgwEWWdfgfhgAWSDS'
-      }
-    }
-  }
+  let(:dashboard_app_response) { File.read('spec/controllers/accounts/apps/dashboard_app_response.json') }
+  let(:webhooks_response) { File.read('spec/controllers/accounts/apps/webhooks_response.json') }
+  let(:inboxes_response) { File.read('spec/integration/use_cases/accounts/apps/chatwoots/inboxes.json') }
+  let(:valid_params) do
+    { apps_chatwoot: {
+      chatwoot_endpoint_url: 'https://chatwoot.test.com/',
+      chatwoot_account_id: '2',
+      chatwoot_user_token: 'ASdasfdgfdgwEWWdfgfhgAWSDS'
+    } }
+  end
+  let(:invalid_params) do
+    { apps_chatwoot: {
+      chatwoot_endpoint_url: 'invalid_url',
+      chatwoot_account_id: '2',
+      chatwoot_user_token: 'ASdasfdgfdgwEWWdfgfhgAWSDS'
+    } }
+  end
   describe 'POST /accounts/{account.id}/apps/chatwoots' do
     before do
       stub_request(:post, /dashboard_apps/)
-      .to_return(body: dashboard_app_response, status: 200, headers: { 'Content-Type' => 'application/json'})
+        .to_return(body: dashboard_app_response, status: 200, headers: { 'Content-Type' => 'application/json' })
       stub_request(:post, /webhooks/)
-      .to_return(body: webhooks_response, status: 200, headers: { 'Content-Type' => 'application/json'})
+        .to_return(body: webhooks_response, status: 200, headers: { 'Content-Type' => 'application/json' })
       stub_request(:get, /inboxes/)
-      .to_return(body: inboxes_response, status: 200, headers: { 'Content-Type' => 'application/json'})
+        .to_return(body: inboxes_response, status: 200, headers: { 'Content-Type' => 'application/json' })
     end
     context 'when is unauthenticated user' do
       it 'returns unauthorized' do
@@ -44,7 +42,7 @@ RSpec.describe Accounts::Apps::ChatwootsController, type: :request do
       before do
         sign_in(user)
       end
-      it 'create app chatwoots' do    
+      it 'create app chatwoots' do
         expect do
           post "/accounts/#{account.id}/apps/chatwoots", params: valid_params
         end.to change(Apps::Chatwoot, :count).by(1)
@@ -58,7 +56,6 @@ RSpec.describe Accounts::Apps::ChatwootsController, type: :request do
         expect(response).to have_http_status(200)
         expect(response.body).to include('Chatwoot endpoint url Invalid Chatwoot configuration')
       end
-      
     end
   end
   describe 'GET /accounts/{account.id}/apps/chatwoots/new' do
@@ -72,7 +69,7 @@ RSpec.describe Accounts::Apps::ChatwootsController, type: :request do
       before do
         sign_in(user)
       end
-      it 'should redirect to new apps chatwoots page' do    
+      it 'should redirect to new apps chatwoots page' do
         get "/accounts/#{account.id}/apps/chatwoots/new"
         expect(response).to have_http_status(200)
         expect(response.body).to include('Salvar')
@@ -90,9 +87,9 @@ RSpec.describe Accounts::Apps::ChatwootsController, type: :request do
   describe 'DELETE /accounts/{account.id}/apps/chatwoots/{chatwoot.id}' do
     before do
       stub_request(:delete, /dashboard_apps/)
-      .to_return(body: '', status: 200, headers: { 'Content-Type' => 'application/json'})
+        .to_return(body: '', status: 200, headers: { 'Content-Type' => 'application/json' })
       stub_request(:delete, /webhooks/)
-      .to_return(body: '', status: 200, headers: { 'Content-Type' => 'application/json'})
+        .to_return(body: '', status: 200, headers: { 'Content-Type' => 'application/json' })
     end
     context 'when is unauthenticated user' do
       it 'returns unautorized' do
@@ -110,14 +107,13 @@ RSpec.describe Accounts::Apps::ChatwootsController, type: :request do
         expect do
           delete "/accounts/#{account.id}/apps/chatwoots/#{chatwoot.id}"
         end.to change(Apps::Chatwoot, :count).by(0)
-        
+
         expect(response).to have_http_status(302)
         expect(response).to redirect_to(account_settings_path(account))
       end
     end
   end
   describe 'UPDATE /accounts/{account.id}/apps/chatwoots/{chatwoot.id}' do
-
     context 'when is unauthenticated user' do
       it 'returns unauthorized' do
         patch "/accounts/#{account.id}/apps/chatwoots/#{chatwoot.id}"
@@ -128,7 +124,7 @@ RSpec.describe Accounts::Apps::ChatwootsController, type: :request do
       before do
         sign_in(user)
       end
-      it 'delete apps chatwoots' do    
+      it 'delete apps chatwoots' do
         patch "/accounts/#{account.id}/apps/chatwoots/#{chatwoot.id}", params: valid_params
         expect(response).to have_http_status(302)
         expect(response).to redirect_to(edit_account_apps_chatwoot_path(account, chatwoot))
@@ -136,4 +132,3 @@ RSpec.describe Accounts::Apps::ChatwootsController, type: :request do
     end
   end
 end
-    
