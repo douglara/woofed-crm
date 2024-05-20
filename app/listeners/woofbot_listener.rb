bee5ff83bfba05a6f79bc54873873c3c690dd6da
Douglas Lara <douglasdelaras@gmail.com>
2024-05-20 14:39:38 -0300

    Change job

--- app/listeners/woofbot_listener.rb
+++ app/listeners/woofbot_listener.rb
@@ -2,6 +2,6 @@
 
 class WoofbotListener
   def event_created(event)
-    Accounts::Contacts::Events::WoofbotJob.perform_later(event.id) if event.account.woofbot_auto_reply?
+    Accounts::Contacts::Events::WoofbotWorker.perform_async(event.id) if event.account.woofbot_auto_reply?
   end
 end
