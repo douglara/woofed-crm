99d6f7d1421c6ddfeb1f0248574ec4257b159028
Yukio Arie <yukioarie@gmail.com>
2024-06-01 02:53:04 -0300

    Add get jwt token frontend

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["source", "defaultMessage", "successMessage"];
+
+  copy() {
+    navigator.clipboard.writeText(this.sourceTarget.value).then(() => {
+      this.showSuccess();
+      setTimeout(() => {
+        this.resetToDefault();
+      }, 2000);
+    });
+  }
+  showSuccess() {
+    this.defaultMessageTarget.classList.add("hidden");
+    this.successMessageTarget.classList.remove("hidden");
+  }
+  resetToDefault() {
+    this.defaultMessageTarget.classList.remove("hidden");
+    this.successMessageTarget.classList.add("hidden");
+  }
+}
