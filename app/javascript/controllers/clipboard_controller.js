1fe960bae57f7ff322eac9a664374bff6f114c1e
Yukio Arie <yukioarie@gmail.com>
2024-06-02 19:54:07 -0300

    Reposition the update user button

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,7 +3,8 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["source", "defaultMessage", "successMessage"];
 
-  copy() {
+  copy(event) {
+    event.preventDefault();
     navigator.clipboard.writeText(this.sourceTarget.value).then(() => {
       this.showSuccess();
       setTimeout(() => {
