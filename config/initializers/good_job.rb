5024b9ddd234ad0bdff7877dc97f707cc0494edd
Yukio Arie <yukioarie@gmail.com>
2024-06-12 20:06:01 -0300

    Add route goodjob authentication

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,7 +1,12 @@
 Rails.application.configure do
   config.good_job.enable_cron = true
   config.good_job.cron = {
-    wpp_connect_refresh_status: { cron: '*/5 * * * *', class: 'Accounts::Apps::WppConnects::Connections::RefreshStatusJob' },
-    evolution_api_refresh_status: { cron: '0 * * * *', class: 'Accounts::Apps::EvolutionApis::Instance::SessionsRefreshStatusJob' },
+    wpp_connect_refresh_status: { cron: '*/5 * * * *',
+                                  class: 'Accounts::Apps::WppConnects::Connections::RefreshStatusJob' },
+    evolution_api_refresh_status: { cron: '0 * * * *',
+                                    class: 'Accounts::Apps::EvolutionApis::Instance::SessionsRefreshStatusJob' }
   }
+end
+GoodJob::Engine.middleware.use Rack::Auth::Basic, 'Restricted Area' do |user, password|
+  [user, password] == [ENV.fetch('MOTOR_AUTH_USERNAME') { 'user' }, ENV.fetch('MOTOR_AUTH_PASSWORD') { 'password' }]
 end
