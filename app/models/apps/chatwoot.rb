b57664c56eb93908385b6e21b28a611272fdfed8
Yukio Arie <yukioarie@gmail.com>
2024-06-19 02:42:14 -0300

    Add missing I18n translations

--- app/models/apps/chatwoot.rb
+++ app/models/apps/chatwoot.rb
@@ -23,21 +23,21 @@ class Apps::Chatwoot < ApplicationRecord
     'inactive': 'inactive',
     'active': 'active',
     'sync': 'sync',
-    'pair': 'pair',
+    'pair': 'pair'
   }
 
   validate :validate_chatwoot, on: :create
   before_destroy :chatwoot_delete_flow
 
   def request_headers
-    {'api_access_token': "#{chatwoot_user_token}", 'Content-Type': 'application/json'}
+    { 'api_access_token': chatwoot_user_token.to_s, 'Content-Type': 'application/json' }
   end
 
   def validate_chatwoot
     chatwoot_create_flow
-    if self.chatwoot_dashboard_app_id.blank? || self.chatwoot_webhook_id.blank?
-      errors.add(:chatwoot_endpoint_url, 'Invalid Chatwoot configuration')
-      errors.add(:chatwoot_user_token, 'Invalid Chatwoot configuration')
+    if chatwoot_dashboard_app_id.blank? || chatwoot_webhook_id.blank?
+      errors.add(:chatwoot_endpoint_url, I18n.t('activerecord.errors.messages.invalid_chatwoot_configuration'))
+      errors.add(:chatwoot_user_token, I18n.t('activerecord.errors.messages.invalid_chatwoot_configuration'))
     end
   end
 
@@ -46,29 +46,30 @@ class Apps::Chatwoot < ApplicationRecord
     dashboard_apps_response = Faraday.post(
       "#{chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot_account_id}/dashboard_apps",
       {
-        "title": "WoofedCRM",
-        "content":[{"type": "frame","url": woofedcrm_embedding_url}]
+        "title": 'WoofedCRM',
+        "content": [{ "type": 'frame', "url": woofedcrm_embedding_url }]
       }.to_json,
-      {'api_access_token': "#{chatwoot_user_token}", 'Content-Type': 'application/json'}
+      { 'api_access_token': chatwoot_user_token.to_s, 'Content-Type': 'application/json' }
     )
 
     webhook_response = Faraday.post(
       "#{chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot_account_id}/webhooks",
       {
-        "webhook":{
+        "webhook": {
           "url": woofedcrm_webhooks_url,
-          "subscriptions":[
-            "contact_created",
-            "contact_updated",
-            "conversation_created",
-            "conversation_status_changed",
-            "conversation_updated",
-            "message_created",
-            "message_updated",
-            "webwidget_triggered"]
+          "subscriptions": %w[
+            contact_created
+            contact_updated
+            conversation_created
+            conversation_status_changed
+            conversation_updated
+            message_created
+            message_updated
+            webwidget_triggered
+          ]
         }
       }.to_json,
-      {'api_access_token': "#{chatwoot_user_token}", 'Content-Type': 'application/json'}
+      { 'api_access_token': chatwoot_user_token.to_s, 'Content-Type': 'application/json' }
     )
 
     self.inboxes = Accounts::Apps::Chatwoots::GetInboxes.call(self)[:ok]
@@ -77,45 +78,43 @@ class Apps::Chatwoot < ApplicationRecord
       dashboard_apps_body = JSON.parse(dashboard_apps_response.body)
       webhook_body = JSON.parse(webhook_response.body)
       self.chatwoot_dashboard_app_id = dashboard_apps_body['id']
-      self.chatwoot_webhook_id =  webhook_body['payload']['webhook']['id']
-      return true
+      self.chatwoot_webhook_id = webhook_body['payload']['webhook']['id']
+      true
     else
-      return false
+      false
     end
-
   rescue Exception => e
-    Rails.logger.error("Chatwoot connection error")
+    Rails.logger.error('Chatwoot connection error')
     Rails.logger.error(e.inspect)
-    return false
+    false
   end
 
   def chatwoot_delete_flow
     dashboard_apps_response = Faraday.delete(
       "#{chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot_account_id}/dashboard_apps/#{chatwoot_dashboard_app_id}",
       {},
-      {'api_access_token': "#{chatwoot_user_token}", 'Content-Type': 'application/json'}
+      { 'api_access_token': chatwoot_user_token.to_s, 'Content-Type': 'application/json' }
     )
 
     webhook_response = Faraday.delete(
       "#{chatwoot_endpoint_url}/api/v1/accounts/#{chatwoot_account_id}/webhooks/#{chatwoot_webhook_id}",
       {},
-      {'api_access_token': "#{chatwoot_user_token}", 'Content-Type': 'application/json'}
+      { 'api_access_token': chatwoot_user_token.to_s, 'Content-Type': 'application/json' }
     )
 
-
-    return true
-    rescue
-      return true
+    true
+  rescue StandardError
+    true
   end
 
   private
 
   def woofedcrm_webhooks_url
-    "#{ENV['FRONTEND_URL']}/apps/chatwoots/webhooks?token=#{self.embedding_token}"
+    "#{ENV['FRONTEND_URL']}/apps/chatwoots/webhooks?token=#{embedding_token}"
   end
 
   def woofedcrm_embedding_url
-    "#{ENV['FRONTEND_URL']}/apps/chatwoots/embedding?token=#{self.embedding_token}"
+    "#{ENV['FRONTEND_URL']}/apps/chatwoots/embedding?token=#{embedding_token}"
   end
 
   def generate_token
--- app/use_cases/accounts/apps/chatwoots/create.rb
+++ app/use_cases/accounts/apps/chatwoots/create.rb
@@ -1,12 +1,12 @@
 class Accounts::Apps::Chatwoots::Create
   def self.call(account, chatwoot_params)
-		chatwoot = account.apps_chatwoots.build(chatwoot_params)
-		if chatwoot.save
-			Accounts::Apps::Chatwoots::SyncImportContactsWorker.perform_async(chatwoot.id)
+    chatwoot = account.apps_chatwoots.build(chatwoot_params)
+    if chatwoot.save
+      Accounts::Apps::Chatwoots::SyncImportContactsWorker.perform_async(chatwoot.id)
       Accounts::Apps::Chatwoots::SyncExportContactsWorker.perform_async(account.id)
-			return { ok: chatwoot }
-		else
-			return { error: chatwoot }
-		end
+      { ok: chatwoot }
+    else
+      { error: chatwoot }
+    end
   end
 end
