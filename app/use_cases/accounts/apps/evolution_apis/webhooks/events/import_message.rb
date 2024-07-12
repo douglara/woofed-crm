01cc9d830d76838cef8234b4d468305e9f62f8d7
Yukio Arie <yukioarie@gmail.com>
2024-07-12 00:40:55 -0300

    Refactor import message evolution api

--- app/use_cases/accounts/apps/evolution_apis/webhooks/events/import_message.rb
+++ app/use_cases/accounts/apps/evolution_apis/webhooks/events/import_message.rb
@@ -1,7 +1,7 @@
 class Accounts::Apps::EvolutionApis::Webhooks::Events::ImportMessage
   def self.call(evolution_api, webhook, content)
     if evolution_api.connected?
-      response = Accounts::Apps::EvolutionApis::Message::Import.call(evolution_api, webhook, content)
+      response = Accounts::Apps::EvolutionApis::Message::Import.new(evolution_api, webhook, content).call
       { ok: response }
     end
   end
