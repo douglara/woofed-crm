e857bfa51011a7052f4aae0a7223544e9b2a5f00
Yukio Arie <yukioarie@gmail.com>
2024-07-26 23:44:28 -0300

    Remove wpp connect from initializer sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,16 +4,16 @@ require 'sidekiq/web'
 redis_max_connections = 12
 
 class RailsLoggerSidekiqServerMiddleware
-  def call(worker, job_options, queue)
+  def call(worker, _job_options, _queue, &block)
     tags = [worker.class.name, worker.jid]
-    Rails.logger.tagged(*tags) { yield }
+    Rails.logger.tagged(*tags, &block)
   end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-      url: ENV['REDIS_URL'] || ENV['REDISCLOUD_URL'],
-      size: redis_max_connections
+    url: ENV['REDIS_URL'] || ENV['REDISCLOUD_URL'],
+    size: redis_max_connections
   }
 end
 
@@ -22,10 +22,10 @@ Sidekiq.configure_server do |config|
     chain.add RailsLoggerSidekiqServerMiddleware
   end
 
-  config.death_handlers << ->(job, ex) do
-    Rails.logger.error("Error #{job['class']} #{job["jid"]} just died with error #{ex.message}.")
+  config.death_handlers << lambda { |job, ex|
+    Rails.logger.error("Error #{job['class']} #{job['jid']} just died with error #{ex.message}.")
     Rails.logger.error(job.inspect)
-  end
+  }
 
   config.redis = {
     url: ENV['REDIS_URL'] || ENV['REDISCLOUD_URL'],
@@ -34,11 +34,12 @@ Sidekiq.configure_server do |config|
 
   if ENV['PREVIEW_APP'].present?
     @activity_kind_whatsapp ||= ActivityKind.find_by_key('whatsapp')
-    Faraday.get("#{@activity_kind_whatsapp['settings']['endpoint_url']}")
+    Faraday.get((@activity_kind_whatsapp['settings']['endpoint_url']).to_s)
   end
-  FlowItems::ActivitiesKinds::WpConnect::Connection::StartAll.call()
 end
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
-  [user, password] == [ENV.fetch('MOTOR_AUTH_USERNAME'){ 'lovewoofed' } , ENV.fetch('MOTOR_AUTH_PASSWORD'){ 'lovewoofed' }]
+  [user, password] == [ENV.fetch('MOTOR_AUTH_USERNAME') { 'lovewoofed' }, ENV.fetch('MOTOR_AUTH_PASSWORD') do
+                                                                            'lovewoofed'
+                                                                          end]
 end
