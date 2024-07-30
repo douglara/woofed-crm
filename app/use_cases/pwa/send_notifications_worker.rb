b3e4c3d3378d16eb6a5225158392f0a35b22ffed
Yukio <45243355+YukioArie@users.noreply.github.com>
2024-07-30 01:26:20 -0300

    Update app/use_cases/pwa/send_notifications_worker.rb
    Co-authored-by: Douglas Lara <douglasdelaras@gmail.com>

--- app/use_cases/pwa/send_notifications_worker.rb
+++ app/use_cases/pwa/send_notifications_worker.rb
@@ -1,5 +1,6 @@
 class Pwa::SendNotificationsWorker < ApplicationJob
   self.queue_adapter = :good_job
+
   def perform(event_id)
     event = Event.find(event_id)
     if event.present? && event.should_delivery_message_scheduled?
