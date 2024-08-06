60fdde4dcf33f97d92bf8f5855193643ef49691b
Douglas Lara <douglasdelaras@gmail.com>
2024-08-06 20:18:40 -0300

    Update dot env rails deprecated method

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-Dotenv::Railtie.load
+Dotenv::Rails.load
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
