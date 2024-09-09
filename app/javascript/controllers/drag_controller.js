8ec7006c69fa3f36dcb8960bb9454674c567161a
Yukio Arie <yukioarie@gmail.com>
2024-09-08 22:20:06 -0300

    Fix animation kanban

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -1,31 +1,36 @@
-import { Controller } from "stimulus"
-import Sortable from "sortablejs"
-import Rails from '@rails/ujs';
+import { Controller } from "stimulus";
+import Sortable from "sortablejs";
+import Rails from "@rails/ujs";
 export default class extends Controller {
-
   connect() {
-    this.initialise()
+    this.sort();
   }
 
-  initialise() {
+  sort() {
     this.sortable = Sortable.create(this.element, {
+      animation: 150,
       group: "pipeline",
-      onEnd: this.end.bind(this)
-    })
+      onAdd: this.end.bind(this),
+      onSort: this.end.bind(this),
+    });
   }
 
   async end(event) {
-    const id = event.item.dataset.id
-    const accountId = event.item.dataset.accountId
-    const to_id = event.to.dataset.id
-    let data = new FormData()
-
-    data.append("deal[position]", event.newIndex + 1)
-    data.append("deal[stage_id]", to_id)
+    event.from.classList.add("pointer-events-none");
+    event.to.classList.add("pointer-events-none");
+    const id = event.item.dataset.id;
+    const accountId = event.item.dataset.accountId;
+    const to_id = event.to.dataset.id;
+    let data = new FormData();
+    data.append("deal[position]", event.newIndex + 1);
+    data.append("deal[stage_id]", to_id);
     Rails.ajax({
-      url: this.data.get("url").replace(":deal_id", id).replace(":account_id", accountId),
-      type: 'PATCH',
-      data: data
-    })
+      url: this.data
+        .get("url")
+        .replace(":deal_id", id)
+        .replace(":account_id", accountId),
+      type: "PATCH",
+      data: data,
+    });
   }
 }
