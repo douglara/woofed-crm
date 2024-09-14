b97ead4957b148904f7927d384aea503766d3721
Douglas Lara <douglasdelaras@gmail.com>
2024-09-14 01:55:00 -0300

    Add tests

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -2,7 +2,7 @@ class Installation < ApplicationRecord
   self.abstract_class = true
 
   def self.installation_url
-    "https://store.woofedcrm.com/installations/new?installation_params=#{{ url: ENV['FRONTEND_URL'],
+    "https://store.woofedcrm.com/installations/new?installation_params=#{{ url: ENV.fetch('FRONTEND_URL', 'http://localhost:3001'),
                                                                            kind: :self_hosted }.to_json}"
   end
 
