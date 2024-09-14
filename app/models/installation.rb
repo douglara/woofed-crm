50d371f96af3050cc7f191669c5dedabb2fa737c
Douglas Lara <douglasdelaras@gmail.com>
2024-09-14 01:21:12 -0300

    Remove unused codes

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,21 +1,12 @@
-# == Schema Information
-#
-# Table name: installations
-#
-#  id                :string           not null, primary key
-#  app_private_key   :string
-#  server_public_key :string
-#  token             :string
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#
 class Installation < ApplicationRecord
+  self.abstract_class = true
+
   def self.installation_url
     "https://store.woofedcrm.com/installations/new?installation_params=#{{ url: ENV['FRONTEND_URL'],
                                                                            kind: :self_hosted }.to_json}"
   end
 
   def self.installation_flow?
-    ENV['ENABLE_WOOFEDSTORE'] == 'true' && Installation.first.blank?
+    User.first.blank?
   end
 end
