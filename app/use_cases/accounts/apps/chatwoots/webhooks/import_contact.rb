7cb9ad63bea7126d498bcc69e8a2c9c98bee3b67
Yukio Arie <yukioarie@gmail.com>
2025-03-25 01:00:09 -0300

    Fix sync chatwoot contact duplicate

--- app/use_cases/accounts/apps/chatwoots/webhooks/import_contact.rb
+++ app/use_cases/accounts/apps/chatwoots/webhooks/import_contact.rb
@@ -5,9 +5,8 @@ class Accounts::Apps::Chatwoots::Webhooks::ImportContact
   end
 
   def self.get_or_import_contact(chatwoot, contact_id)
-    contact = chatwoot.account.contacts.where(
-      "additional_attributes->>'chatwoot_id' = ?", contact_id.to_s
-    ).first
+    contact = chatwoot.account.contacts.where("CAST(additional_attributes->>'chatwoot_id' AS INTEGER) = ?",
+                                              contact_id).first
     contact_att = get_contact(chatwoot, contact_id)
     return 'Contact not found' if contact_att == false
 
