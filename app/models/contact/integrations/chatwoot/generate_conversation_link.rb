9a5fb8e595f50eb64857783285b9c9701617f8bb
Yukio Arie <yukioarie@gmail.com>
2025-05-02 02:06:16 -0300

    Improve name error from GenerateConversationLink

--- app/models/contact/integrations/chatwoot/generate_conversation_link.rb
+++ app/models/contact/integrations/chatwoot/generate_conversation_link.rb
@@ -7,12 +7,10 @@ class Contact::Integrations::Chatwoot::GenerateConversationLink
     chatwoot = fetch_chatwoot_for_account
     chatwoot_contact_id = @contact.additional_attributes['chatwoot_id']
 
-    unless chatwoot && chatwoot_contact_id
-      return { error: 'There is no chatwoot integration or chatwoot_id on contact' }
-    end
+    return { error: 'no_chatwoot_or_id' } unless chatwoot && chatwoot_contact_id
 
     conversation_id = fetch_conversation_id(chatwoot, chatwoot_contact_id)
-    return { error: "There is no conversation on contact id:#{chatwoot_contact_id}" } unless conversation_id
+    return { error: 'no_conversation' } unless conversation_id
 
     { ok: build_conversation_url(chatwoot, conversation_id) }
   end
