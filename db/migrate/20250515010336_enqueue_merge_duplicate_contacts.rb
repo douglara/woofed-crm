372fc8ef51754cf6aa77580727a6e80e216649b7
Yukio Arie <yukioarie@gmail.com>
2025-05-21 09:05:41 -0300

    Remove unnecessary migration EnqueueMergeDuplicateContacts

--- db/migrate/20250515010336_enqueue_merge_duplicate_contacts.rb
+++
@@ -1,6 +0,0 @@
-class EnqueueMergeDuplicateContacts < ActiveRecord::Migration[7.1]
-  def change
-    processed_groups = Contact::Migrations::MergeDuplicateContacts.new(enqueue: true).call
-    Rails.logger.info("Enqueued #{processed_groups} groups of duplicate contacts for merging")
-  end
-end
