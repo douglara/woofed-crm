6a6f6781f78753e2c2d8c06d7eb446d0bd316457
Henrique Costa <henrique.costa@flowih.com>
2025-05-25 01:06:07 -0300

    chore: atualizar o campo 'language' para 'pt_BR:en' nos modelos de usuário e nas fábricas - Ajusta a configuração de idiomas para refletir a nova configuração padrão

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,7 +8,7 @@
 #  encrypted_password     :string           default(""), not null
 #  full_name              :string           default(""), not null
 #  job_description        :string           default("other"), not null
-#  language               :string           default(""), not null
+#  language               :string           default("pt_BR:en"), not null
 #  notifications          :jsonb            not null
 #  phone                  :string
 #  remember_created_at    :datetime
