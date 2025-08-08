b83ea79cb698bd70ba070060c8c3fa02d2aba7d1
Yukio Arie <yukioarie@gmail.com>
2025-08-07 23:49:59 -0300

    Add new components, componentize modal and drawer, fix navs height

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -5,7 +5,9 @@ export default class extends Controller {
   connect() {
 		this.modal = new Modal(this.element, {
 			closable: false,
-			backdrop: 'static'
+			backdrop: 'static',      
+      backdropClasses:
+        "bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-50 pointer-events-none",
 		})
 		this.modal.show()
   }
