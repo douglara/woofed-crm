86c8600199dd7db922e3791e7383ffc80add7ed3
Douglas Lara <douglasdelaras@gmail.com>
2026-02-07 22:36:53 +1000

    Install Inertia

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+
+  # Redirect to localhost from 127.0.0.1 to use same IP address with Vite server
+  constraints(host: "127.0.0.1") do
+    get "(*path)", to: redirect { |params, req| "#{req.protocol}localhost:#{req.port}/#{params[:path]}" }
+  end
+  get 'inertia-example', to: 'inertia_example#index'
   mount Motor::Admin => '/motor_admin'
   mount Sidekiq::Web => '/sidekiq'
   mount GoodJob::Engine => 'good_job'
