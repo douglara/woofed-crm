f7dca40b88c48083707560a6c06513140bf14daa
Yukio Arie <yukioarie@gmail.com>
2026-02-15 20:06:25 -0300

    Improve and add tests

--- spec/models/deals_spec.rb
+++ spec/models/deals_spec.rb
@@ -26,7 +26,9 @@ RSpec.describe Deal do
         expect(stage1_position2.reload.position).to eq 1
         expect(stage1_position3.reload.position).to eq 2
 
-        expect(stage1_position1.reload.position).to eq 1
+        expect(stage1_position1.reload.stage_id).to eq stage2.id
+
+        expect(stage1_position1.position).to eq 1
         expect(stage2_position1.reload.position).to eq 2
         expect(stage2_position2.reload.position).to eq 3
         expect(stage2_position3.reload.position).to eq 4
@@ -41,8 +43,10 @@ RSpec.describe Deal do
         expect(stage1_position2.reload.position).to eq 1
         expect(stage1_position3.reload.position).to eq 2
 
+        expect(stage1_position1.reload.stage_id).to eq stage2.id
+
         expect(stage2_position1.reload.position).to eq 1
-        expect(stage1_position1.reload.position).to eq 2
+        expect(stage1_position1.position).to eq 2
         expect(stage2_position2.reload.position).to eq 3
         expect(stage2_position3.reload.position).to eq 4
         expect(last_event.kind).to eq('deal_stage_change')
@@ -56,10 +60,29 @@ RSpec.describe Deal do
         expect(stage1_position2.reload.position).to eq 1
         expect(stage1_position3.reload.position).to eq 2
 
+        expect(stage1_position1.reload.stage_id).to eq stage2.id
+
         expect(stage2_position1.reload.position).to eq 1
         expect(stage2_position2.reload.position).to eq 2
-        expect(stage1_position1.reload.position).to eq 3
+        expect(stage1_position1.position).to eq 3
         expect(stage2_position3.reload.position).to eq 4
+        expect(last_event.kind).to eq('deal_stage_change')
+      end
+
+      it 'move stage1_position1 to stage2 position 250 and create deal_stage_change event' do
+        expect do
+          Deal::CreateOrUpdate.new(stage1_position1, { stage: stage2, position: 250 }).call
+        end.to change(Event, :count).by(1)
+
+        expect(stage1_position2.reload.position).to eq 1
+        expect(stage1_position3.reload.position).to eq 2
+
+        expect(stage1_position1.reload.stage_id).to eq stage2.id
+
+        expect(stage2_position1.reload.position).to eq 1
+        expect(stage2_position2.reload.position).to eq 2
+        expect(stage2_position3.reload.position).to eq 3
+        expect(stage1_position1.position).to eq 250
         expect(last_event.kind).to eq('deal_stage_change')
       end
     end
@@ -79,6 +102,32 @@ RSpec.describe Deal do
         expect(stage1_position1.reload.position).to eq 3
         expect(stage1_position2.reload.position).to eq 1
         expect(stage1_position3.reload.position).to eq 2
+      end
+    end
+
+    context 'create a deal' do
+      context 'when position is provided' do
+        it 'places the new deal at the end of the stage' do
+          new_deal = build(:deal, stage: stage1)
+          new_deal = Deal::CreateOrUpdate.new(new_deal, {}).call
+
+          expect(stage1_position1.reload.position).to eq 1
+          expect(stage1_position2.reload.position).to eq 2
+          expect(stage1_position3.reload.position).to eq 3
+          expect(new_deal.reload.position).to eq 4
+        end
+      end
+
+      context 'when position is not provided' do
+        it 'inserts the new deal at the specified position' do
+          new_deal = build(:deal, stage: stage1, position: 2)
+          new_deal = Deal::CreateOrUpdate.new(new_deal, {}).call
+
+          expect(stage1_position1.reload.position).to eq 1
+          expect(new_deal.reload.position).to eq 2
+          expect(stage1_position2.reload.position).to eq 3
+          expect(stage1_position3.reload.position).to eq 4
+        end
       end
     end
   end
