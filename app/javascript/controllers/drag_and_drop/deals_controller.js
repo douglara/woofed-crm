8ea390e97fc92c7d183d217a6210045574240f6b
Yukio Arie <yukioarie@gmail.com>
2026-02-16 16:12:57 -0300

    Refactor drag and drop js files

--- app/javascript/controllers/drag_and_drop/deals_controller.js
+++ app/javascript/controllers/drag_and_drop/deals_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 import Sortable from "sortablejs";
 import { patch } from "@rails/request.js";
+import DropParamsBuilder from "./helpers";
 
 export default class extends Controller {
   connect() {
@@ -74,63 +75,5 @@ export default class extends Controller {
     }
     event.from.classList.remove("pointer-events-none");
     event.to.classList.remove("pointer-events-none");
-  }
-}
-
-class DropParamsBuilder {
-  constructor(event) {
-    this.event = event;
-    this.topElement = event.item.previousElementSibling || null;
-    this.bottomElement = event.item.nextElementSibling || null;
-  }
-  buildDropParams() {
-    if (this.isMovedBetweenStages) {
-      return this.#paramsForNewStage();
-    } else {
-      return this.#paramsInCurrentStage();
-    }
-  }
-  get isMovedBetweenStages() {
-    return this.event.from !== this.event.to;
-  }
-  get movementDirection() {
-    const { oldIndex: startIndex, newIndex: endIndex } = this.event;
-    return endIndex > startIndex ? "down" : "up";
-  }
-  get topElementId() {
-    return this.topElement.dataset.id;
-  }
-  get bottomElementId() {
-    return this.bottomElement.dataset.id;
-  }
-  get selfElementId() {
-    return this.event.item.dataset.id;
-  }
-  get quantityElementsPassed() {
-    return Math.abs(this.event.oldIndex - this.event.newIndex);
-  }
-
-  #paramsForNewStage() {
-    if (this.bottomElement) {
-      return {
-        closest_deal_id: this.bottomElementId,
-        closest_deal_direction: "bottom",
-      };
-    }
-    if (this.topElement) {
-      return {
-        closest_deal_id: this.topElementId,
-        closest_deal_direction: "top",
-      };
-    }
-
-    return { closest_deal_id: this.selfElementId };
-  }
-  #paramsInCurrentStage() {
-    if (this.quantityElementsPassed === 0)
-      return { closest_deal_id: this.selfElementId };
-    return this.movementDirection === "up"
-      ? { closest_deal_id: this.bottomElementId }
-      : { closest_deal_id: this.topElementId };
   }
 }
