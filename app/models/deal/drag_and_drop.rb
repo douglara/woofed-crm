809c225c35ccbf6209ca85827a88e01806747295
Yukio Arie <yukioarie@gmail.com>
2026-02-17 01:33:03 -0300

    Fix deal drag and drop

--- app/models/deal/drag_and_drop.rb
+++ app/models/deal/drag_and_drop.rb
@@ -1,34 +1,34 @@
 class Deal::DragAndDrop
-  def initialize(deal:, position:, new_stage: nil, closest_deal_direction: nil)
+  def initialize(deal:, position:, new_stage_id: nil, element_reference_direction: nil)
     raise ArgumentError, 'deal is required' unless deal
     raise ArgumentError, 'position is required' unless position
 
     @deal = deal
-    @new_stage = new_stage
+    @new_stage_id = new_stage_id
     @position = position
-    @closest_deal_direction = closest_deal_direction
+    @element_reference_direction = element_reference_direction
   end
 
   def call
-    validate_direction
+    return { error: 'invalid direction' } unless validate_direction
 
-    Deal::CreateOrUpdate.new(deal, deal_params).call
+    { ok: Deal::CreateOrUpdate.new(deal, deal_params).call }
   end
 
   private
 
-  attr_reader :deal, :new_stage, :position, :closest_deal_direction
+  attr_reader :deal, :new_stage_id, :position, :element_reference_direction
 
   def deal_params
-    return { stage_id: new_stage.id, position: new_position } if move_between_stages?
+    return { stage_id: new_stage_id, position: new_position } if move_between_stages?
 
     { position: new_position }
   end
 
   def new_position
-    return position unless move_between_stages? || closest_deal_direction.nil?
+    return position unless move_between_stages? || element_reference_direction.present?
 
-    return position + 1 if closest_deal_direction == 'bottom'
+    return position + 1 if element_reference_direction == 'bottom'
 
     return 1 if position == 1
 
@@ -36,10 +36,10 @@ class Deal::DragAndDrop
   end
 
   def validate_direction
-    raise ArgumentError, 'invalid direction' unless [nil, 'bottom', 'top'].include?(closest_deal_direction)
+    [nil, 'bottom', 'top'].include?(element_reference_direction)
   end
 
   def move_between_stages?
-    new_stage.present? && new_stage != deal.stage
+    new_stage_id.present? && new_stage_id != deal.stage_id.to_s
   end
 end
