32df149acd368cc71bede9e4f5c01085cbbe74b7
Yukio Arie <yukioarie@gmail.com>
2026-02-26 05:45:34 -0300

    Fix command input style

--- app/javascript/components/ui/command.tsx
+++ app/javascript/components/ui/command.tsx
@@ -42,7 +42,7 @@ const CommandInput = React.forwardRef<
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
-        "flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+        "flex h-10 w-full rounded-md bg-transparent py-3 px-0 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 border-0 shadow-none focus:ring-0 focus:border-0 focus:outline-none",
         className
       )}
       {...props}
