7a759c7085ee3935f6cb3031cda7eff70f3f84f9
Douglas Lara <douglasdelaras@gmail.com>
2026-03-22 18:24:17 +1000

    Fix /up health check blocks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,14 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = {
+    redirect: {
+      exclude: ->(request) { request.path == '/up' }
+    }
+  }
+  config.host_authorization = {
+    exclude: ->(request) { request.path == '/up' }
+  }
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
