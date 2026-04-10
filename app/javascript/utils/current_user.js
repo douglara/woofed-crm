c104ba055bc00bcd0c2946ce849c3a7352899aef
Yukio Arie <yukioarie@gmail.com>
2026-04-09 23:38:09 -0300

    Refactor current_user js

--- app/javascript/utils/current_user.js
+++ app/javascript/utils/current_user.js
@@ -1,16 +1,7 @@
 import { getMetaJSON } from "./meta";
 
-let cachedUser = null;
-
-function loadCurrentUser() {
-  if (!cachedUser) {
-    cachedUser = getMetaJSON("user-data") || {};
-  }
-  return cachedUser;
-}
-
 export function currentUser() {
-  return loadCurrentUser();
+  return getMetaJSON("user-data") || {};
 }
 
 export function isLoggedIn() {
