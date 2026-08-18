8e0491505521bdb3991e60b4a9cb24c943a4b861
Yukio Arie <yukioarie@gmail.com>
2026-08-17 23:19:26 -0300

    Fix salesforce page exhibition

--- spec/controllers/accounts/settings_controller_spec.rb
+++ spec/controllers/accounts/settings_controller_spec.rb
@@ -22,6 +22,16 @@ RSpec.describe Accounts::SettingsController, type: :request do
         expect(response).to have_http_status(:success)
         expect(flash[:error]).to be_nil
       end
+
+      # Salesforce is an Inertia page. Turbo Drive swaps only the <body>, so it
+      # would leave the React root unmounted and the page blank until a manual
+      # reload -- the link has to fall back to a full browser navigation.
+      it 'renders the Salesforce link opted out of Turbo' do
+        get "/accounts/#{account.id}/settings"
+
+        link = Nokogiri::HTML(response.body).at_css("a[href='#{account_apps_salesforce_path(account)}']")
+        expect(link['data-turbo']).to eq('false')
+      end
     end
   end
 end
